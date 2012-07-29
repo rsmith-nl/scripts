518a59268adbfba25d60eb588139f865ecdd58ea
Roland Smith <rsmith@xs4all.nl>
2012-07-29 18:39:01 +0200

    Update mkhistory.py from logbook version.

--- mkhistory.py
+++ mkhistory.py
@@ -1,74 +1,89 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Adds my copyright notice to photos.
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Time-stamp: <2012-04-28 11:17:09 rsmith>
+# $Date: 2012-07-29$
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to mkhistory.py. This work is published from
 # the Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
 
-'Script to format a Git log for LaTeX'
+'''Script to format a Git log for LaTeX'''
 
 import subprocess
 import sys
 import os
 
-def header():
-    rv = '% -*- latex -*-\n'
-    rv += '% Automatisch gegenereerd door tools/mkhistory.py\n\n'
-    rv += '\chapter{Wijzigingen}\n\n'
-    rv += 'Dit hoofdstuk wordt automatisch gegenereerd uit het '
-    rv += '\\texttt{git} revisiecontrolesysteem.\\\\\n'
-    rv += 'De meest recente wijzigingen staan bovenaan.\n\n'
-    return rv
+# The following texts determine how the commits are generated. Change them to
+# suit your preferences.
+header = r'''% -*- latex -*-
+% Automatisch gegenereerd door tools/mkhistory.py
+
+\chapter{Wijzigingen}
+
+Dit hoofdstuk wordt automatisch gegenereerd uit het \texttt{git}
+revisiecontrolesysteem. De meest recente wijzigingen staan bovenaan.
+
+'''
+commithdr = r'wijziging:'
+authorhdr = r'door:'
+datehdr = r'datum:'
+# If you change the commithdr, authorhdr or datehdr, you might have to change
+# the widths in the begintable text below.
+begintable = r'''\begingroup\hspace{-\tabcolsep}
+\begin{tabular}{p{0.1\textwidth}p{0.8\textwidth}}
+'''
+endtable = r'''\end{tabular}\endgroup
+
+'''
 
 def genrecords(lol):
     rv = ''
     for ln in lol:
         if ln.startswith('commit'):
             if rv:
-                rv += '\end{tabular}\n\n'
+                rv += endtable
                 yield rv
-            rv = '\\begin{tabular}{p{0.07\\textwidth}p{0.87\\textwidth}}\n'
+            rv = begintable
             words = ln.split(' ', 1)
             ln = ln.replace(' ', ' & ', 1)
-            rv += '  ' + words[0] + ' & ' + '\\texttt{'
-            rv += words[1] + '}\\\\\n'
+            rv += '  ' + commithdr + ' & ' + words[1] + '\\\\\n'
         elif ln.startswith('Merge:'):
             words = ln.split(':', 1)
-            rv += '  ' + words[0] + ': & ' + '\\texttt{'
-            rv += words[1].lstrip(None) + '}\\\\\n'
-        elif (ln.startswith('Author:') or
-              ln.startswith('Date:')) :
+            rv += '  ' + commithdr + ': & ' + words[1].lstrip(None) + '\\\\\n'
+        elif ln.startswith('Author:'):
             ln = ln.replace(': ', ': & ')
+            ln = ln.replace('Author:', authorhdr)
+            rv += '  ' + ln + '\\\\\n'
+        elif ln.startswith('Date:'):
+            ln = ln.replace(': ', ': & ')
+            ln = ln.replace('Date:', datehdr)
             rv += '  ' + ln + '\\\\\n'
         else:
             ln = ln.lstrip(None)
             if len(ln):
                 rv += '  & '+ ln + '\\\\\n'
     if rv:
-        rv += '\end{tabular}\n\n% EOF\n'
+        rv += endtable + '% EOF\n'
         yield rv
     return
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         path, binary = os.path.split(sys.argv[0])
-        print "Gebruik: {} bestandsnaam".format(binary)
+        print "Usage: {} outputfilename".format(binary)
         exit(0)
     fn = sys.argv[1]
     try:
         lines = subprocess.check_output(['git', 'log']).split('\n')
     except  CalledProcessError:
-        print "Git niet gevonden! Stop."
-        exit(1)
+        print "Git not found! Stop."
+        sys.exit(1)
     if fn == '-':
         of = sys.stdout
     else:
         of = open(fn, 'w+')
-    of.write(header())
+    of.write(header)
     for rec in genrecords(lines):
         of.write(rec)
     of.close()
