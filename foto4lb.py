9fc16e02a06abd804e045f484b612816f2cc74f8
Roland Smith <rsmith@xs4all.nl>
2012-08-31 21:21:38 +0200

    Replace time-stamps by data keywords.

--- foto4lb.py
+++ foto4lb.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Time-stamp: <2012-08-05 13:40:10 rsmith>
+# $Date$
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to foto4lb.py. This work is published from the
