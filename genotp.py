7f66ab28cee027c80e8db70a1b8f3798f3cf4a8b
Roland Smith <rsmith@xs4all.nl>
2014-03-08 14:31:03 +0100

    Modularized genotp.
    - Moved the one-time pad generation to a function.
    - Also added a unique ID to the key.
    - Used a main function.

--- genotp.py
+++ genotp.py
@@ -11,17 +11,39 @@
 """Generates a one-time pad."""
 
 from __future__ import division, print_function
+from datetime import datetime
 from os import urandom
 
 
 def rndcaps(n):
-    """Generates a string of n random capital letters."""
+    """Generates a string of random capital letters.
+
+    :param n: length of the output string
+    :returns: a string of n random capital letters.
+    """
     b = urandom(n)
     return ''.join([chr(int(round(ord(c)/10.2))+65) for c in b])
 
 
+def otp(n=65):
+    """Return a one-time pad.
+
+    :param n: number of lines in the key
+    :returns: 65 lines of 12 groups of 5 random capital letters.
+    """
+    lines = []
+    for num in range(1, 66):
+        i = ['{:02d} '.format(num)]
+        i += [rndcaps(5) for j in range(0, 12)]
+        lines.append(' '.join(i))
+    return '\n'.join(lines)
+
+
+def main():
+    ident = '>> {}, {} <<'
+    print(ident.format(rndcaps(10), datetime.utcnow()))
+    print(otp())
+
+
 if __name__ == '__main__':
-    for num in range(1, 67):
-        ls = ['{:02d} '.format(num)]
-        cb = [rndcaps(5) for j in range(0, 12)]
-        print(' '.join(ls+cb))
+    main()
