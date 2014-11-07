d95f0c858098b2f46ca9f092b405d7ff5ccc56cc
Roland Smith <rsmith@xs4all.nl>
2014-11-07 20:52:14 +0100

    Ported vid2mkv.py to Python 3.4.

--- vid2mkv.py
+++ vid2mkv.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3.4
+# vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # $Date$
@@ -10,8 +10,6 @@
 
 """Convert all video files given on the command line to Theora/Vorbis streams
 in a Matroska container."""
-
-from __future__ import print_function, division
 
 __version__ = '$Revision$'[11:-2]
 
