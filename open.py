fe31d043e033572c8f41822dff189caa0f71b170
Roland Smith <rsmith@xs4all.nl>
2014-12-26 13:36:19 +0100

    Added new “open” script.
    This script is inspired by a Mac OS X program of the same name.

---
+++ open.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# file: open.py
+# vim:fileencoding=utf-8:ft=python
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2014-12-26 11:45:59 +0100
+# $Date$
+# $Revision$
+
+"""Opens the files given on the command line in the approriate program."""
+
+from re import search, IGNORECASE
+from subprocess import Popen, check_output
+from sys import argv
+
+__version__ = '$Revision$'[11:-2]
+
+matches = {'\.pdf$': ['mupdf'], '\.html$': ['firefox', '-new-tab'],
+           '\.zip$': ['unzip', '-l'], '\.xcf$': ['gimp'],
+           '\.(jpg|jpeg|png|gif|tif)$': ['gpicview'],
+           '\.(tar\.|t)([zZ]|gz|bz[2]?|xz)$': ['tar', 'tf']}
+
+
+def findprog(matchdict, fname):
+    """For the given filename, returns the matching program.
+
+    :param matchdict: dictionary of regex:[arguments]
+    :param fname: file name
+    :returns: the commands for subprocess.Open.
+    """
+    for k, v in matchdict.items():
+        if search(k, fname, IGNORECASE) is not None:
+            return v + [fname]
+    try:
+        if b'text' in check_output(['file', fname]):
+            return ['gvim', '--nofork', fname]
+    except CalledProcessError:
+        pass
+    return None
+
+
+def main(args):
+    """Entry point for this script.
+
+    :param args: command line arguments
+    """
+    if len(args) == 1:
+        binary = os.path.basename(args[0])
+        print("{} ver. {}".format(binary, __version__), file=sys.stderr)
+        print("Usage: {} [file ...]".format(binary), file=sys.stderr)
+        sys.exit(0)
+    del args[0]  # delete the name of the script.
+    # Real work starts here.
+    for fn in args:
+        cmds = findprog(matches, fn)
+        if cmds is not None:
+            Popen(cmds)
+
+
+if __name__ == '__main__':
+    main(argv)
