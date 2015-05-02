4dbe2a0127fe269049235fb8bbb3c47fab3f2fb1
Roland Smith <rsmith@xs4all.nl>
2015-05-02 09:41:15 +0200

    Added IPython start (“ips”) script.

---
+++ ips.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# file: ips.sh
+# vim:fileencoding=utf-8:ft=sh
+# Start a new terminal running IPython.
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2015-05-02 09:27:08 +0200
+
+urxvt -title IPython -e ipython &
