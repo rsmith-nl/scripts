f1b6e848ec2d901f29f77831c685f30cd29e27dc
Roland Smith <rsmith@xs4all.nl>
2015-05-03 22:55:27 +0200

    General keyword clean-up in Python scripts.
    - Replaced date keywords by 'Last modified'.
    - Changed __version__ to 1.0.0 in all python files. All of them are in use, so
      might as well. :-)
    - Made property.py compatible with Python 3.
    - Truncate the file before writing back changed contents in py-ver.py to
      prevent artefacts at the end of the file.

--- mkhistory.py
+++ mkhistory.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# $Date$
+# Last modified: 2015-05-03 22:10:01 +0200
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to mkhistory.py. This work is published from
