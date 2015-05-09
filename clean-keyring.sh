29abc1688d036d12fe13c5ecb9d5a87d098b1f33
Roland Smith <rsmith@xs4all.nl>
2015-05-09 15:11:36 +0200

    Modified clean-keyring so that it works.

--- clean-keyring.sh
+++ clean-keyring.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Clean up the GPG Keyring.  Keep it tidy.
-# blog.lavall.ee
+# www.lavall.ee
+#
+# Modified by Roland Smith.
+# Last modified: 2015-05-09 15:09:30 +0200
 
 echo -n "Expired Keys: "
-for expiredKey in $(gpg2 --list-keys | awk '/^pub.* \[expired\: / {id=$2; sub(/^.*\//, "", id); print id}' | fmt -w 999 ); do
+for expiredKey in $(gpg2 -k | awk '/^pub.* \[expired\: / {id=$2; sub(/^.*\//, "", id); print id}'); do
     echo -n "$expiredKey"
-    gpg2 --batch --quiet --delete-keys $expiredKey >/dev/null 2>&1
+    gpg2 --batch --yes --delete-keys $expiredKey >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -n "(OK), "
     else
@@ -15,7 +18,7 @@ done
 echo done.
 
 echo -n "Update Keys: "
-for keyid in $(gpg -k | grep ^pub | grep -v expired: | grep -v revoked: | cut -d/ -f2 | cut -d' ' -f1); do
+for keyid in $(gpg2 -k | awk '/^pub/&&!(/revoked\:/||/expired\:/) {id=$2; sub(/^.*\//, "", id); print id}'); do
     echo -n "$keyid"
     gpg2 --batch --quiet --edit-key "$keyid" check clean cross-certify save quit > /dev/null 2>&1
     if [ $? -eq 0 ]; then
