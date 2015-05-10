f9abb650d2355831d6ee3618153936bf022c328c
Roland Smith <rsmith@xs4all.nl>
2015-05-10 15:52:06 +0200

    Fixed error message in find-modified.sh.

--- find-modified.sh
+++ find-modified.sh
@@ -2,7 +2,7 @@
 # finds files that have been modified at most $1 days ago.
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# $Date$
+# Last modified: 2015-05-10 15:52:45 +0200
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to find-modified. This work is published from
@@ -11,7 +11,7 @@
 if [ -z $1 ]; then
    echo "Error: you must give a number of days as the argument."
    echo "usage: $(basename $0) [number]"
-   echo "This finds the files from your home directory downwards,"
+   echo "This finds the files from the current directory downwards,"
    echo "that have been modified up to [number] days ago."
    exit 0
 fi
