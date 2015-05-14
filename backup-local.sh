83bac0d28cd947f6ee7210f930770313eccb0536
Roland Smith <rsmith@xs4all.nl>
2015-05-14 21:06:16 +0200

    Added comments to backup-local.sh.

--- backup-local.sh
+++ backup-local.sh
@@ -2,9 +2,12 @@
 # file: usr/local/sbin/backup-local
 # vim:fileencoding=utf-8:ft=sh
 #
+# Backs up mount points to other mount points.
+# This script is designed to be run from cron.
+#
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2008-05-26 22:59:14 +0200
-# Last modified: 2015-05-09 12:26:44 +0200
+# Last modified: 2015-05-14 20:10:14 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to backup-local. This work is published
@@ -23,19 +26,28 @@ done
 FLAGS="-axq -H --delete"
 LOG="logger -t 'backup-local'"
 
-# This script assumes that the backups are not mounted.
-
+# __mkbackup origin dest
+#
+# Backs up a mount point to another mount point.
+# This function assumes that the backups are *not* mounted.
+#
+# Arguments:
+#     origin: mount point to copy.
+#     dest: mount point to back up to.
+#
+# Requires:
+#   * rsync
 __mkbackup() { # 1=origin 2=dest
     mount ${2}
     if df|grep ${2} >/dev/null; then
         rsync $FLAGS ${1%/}/ ${2} && $LOG "${1} successfully backed-up."
         umount ${2}
     else
-        "Backup for ${1} not mounted! Not backed up."
+        $LOG "Backup for ${1} not mounted! Not backed up."
     fi
 }
 
-# The main program
+# The main program;
 __mkbackup / /mnt/bk/root
 __mkbackup /usr /mnt/bk/usr
 __mkbackup /var /mnt/bk/var
