1dac39395c1989cc9e837a66b613e9d3233731e1
Roland Smith <rsmith@xs4all.nl>
2015-05-25 13:17:33 +0200

    Reorganized img4latex.py. Improved docstrings.

--- img4latex.py
+++ img4latex.py
@@ -4,7 +4,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2014-12-04 20:14:34 +0100
-# Last modified: 2015-05-03 22:06:06 +0200
+# Last modified: 2015-05-25 13:15:16 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to img4latex.py. This work is published
@@ -13,7 +13,7 @@
 """Program to check a PDF, PNG or JPEG file and return
    a suitable LaTeX figure environment for it."""
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 import argparse
 import os
@@ -21,12 +21,90 @@ import subprocess
 import sys
 
 
+def main(argv):
+    """Entry point for img4latex.
+
+    Arguments:
+        argv: All command line arguments.
+    """
+    checkfor(['gs', '-v'])
+    checkfor(['identify', '-version'])
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-w', '--width',
+                        default=160,
+                        type=float,
+                        help='width of the text block in mm. (default 160)')
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=__version__)
+    parser.add_argument('file', nargs='*')
+    args = parser.parse_args(argv)
+    args.width *= 72 / 25.4  # convert to points
+    del args.file[0]
+    if not args.file:
+        parser.print_help()
+        sys.exit(0)
+    for filename in args.file:
+        if not os.path.exists(filename):
+            print('File "{}" does not exist.'.format(filename))
+            continue
+        if filename.endswith(('.ps', '.PS', '.eps', '.EPS', '.pdf', '.PDF')):
+            bbox = getpdfbb(filename)
+            bbwidth = float(bbox[2]) - float(bbox[0])
+            scale = 1.0
+            if bbwidth > args.width:
+                scale = args.width / bbwidth
+            fs = '[viewport={} {} {} {},clip,scale={s:.3f}]'
+            opts = fs.format(*bbox, s=scale)
+        elif filename.endswith(('.png', '.PNG', '.jpg', '.JPG', '.jpeg',
+                                '.JPEG')):
+            width = getpicwidth(filename)
+            opts = ''
+            if width > args.width:
+                opts = '[scale={:.3f}]'.format(args.width / width)
+        else:
+            fskip = 'File "{}" has an unrecognized format. Skipping...'
+            print(fskip.format(filename))
+            continue
+        output_figure(filename, opts)
+    print()
+
+
+def checkfor(args, rv=0):
+    """
+    Make sure that a program necessary for using this script is available.
+    If the required utility is not found, this function will exit the program.
+
+    Arguments:
+        args: String or list of strings of commands. A single string may not
+            contain spaces.
+        rv: Expected return value from evoking the command.
+    """
+    if isinstance(args, str):
+        if ' ' in args:
+            raise ValueError('no spaces in single command allowed')
+        args = [args]
+    try:
+        with open(os.devnull, 'w') as bb:
+            rc = subprocess.call(args, stdout=bb, stderr=bb)
+        if rc != rv:
+            raise OSError
+    except OSError as oops:
+        outs = "Required program '{}' not found: {}."
+        print(outs.format(args[0], oops.strerror))
+        sys.exit(1)
+
+
 def getpdfbb(fn):
     """Use ghostscript to get the BoundingBox of a PostScript or PDF
     file.
 
-    :param fn: name of the file to get the BoundingBox from
-    :returns: a tuple of strings in the form (llx lly urx ury).
+    Arguments:
+        fn: Name of the file to get the BoundingBox from.
+
+    Returns:
+        A tuple of strings in the form (llx lly urx ury), where ll means
+        lower left and ur means upper right.
     """
     gsopts = ['gs', '-q', '-dFirstPage=1', '-dLastPage=1', '-dNOPAUSE',
               '-dBATCH', '-sDEVICE=bbox', fn]
@@ -35,11 +113,14 @@ def getpdfbb(fn):
     return bbs.split(' ')[1:]
 
 
-def getpixwidth(fn):
-    """Use ImageMagick to get the width of a bitmapped file
+def getpicwidth(fn):
+    """Use ImageMagick to get the width of a bitmapped file.
 
-    :param fn: name of the file to check.
-    :returns: width in points.
+    Arguments:
+        fn: Name of the file to check.
+
+    Returns:
+        Width of the image in points.
     """
     idargs = ['identify', '-verbose', fn]
     idres = subprocess.check_output(idargs, stderr=subprocess.STDOUT)
@@ -65,8 +146,9 @@ def getpixwidth(fn):
 def output_figure(fn, options=""):
     """Print the LaTeX code for the figure.
 
-    :param fn: name of the file to get the BoundingBox from
-    :param options: options to add to the \includegraphics command
+    Arguments:
+        fn: name of the file.
+        options: options to add to the \includegraphics command.
     """
     fb = fn.rpartition('.')[0]
     fbnodir = fb[fn.rfind('/') + 1:]
@@ -77,77 +159,6 @@ def output_figure(fn, options=""):
     label = fbnodir.replace(' ', '-').replace('_', '-')
     print(r'  \caption{{\label{{fig:{}}}{}}}'.format(label, label))
     print(r'\end{figure}')
-
-
-def checkfor(args, rv=0):
-    """Make sure that a program necessary for using this script is
-    available.
-
-    :param args: String or list of strings of commands. A single string may
-    not contain spaces.
-    :param rv: Expected return value from evoking the command.
-    """
-    if isinstance(args, str):
-        if ' ' in args:
-            raise ValueError('no spaces in single command allowed')
-        args = [args]
-    try:
-        with open(os.devnull, 'w') as bb:
-            rc = subprocess.call(args, stdout=bb, stderr=bb)
-        if rc != rv:
-            raise OSError
-    except OSError as oops:
-        outs = "Required program '{}' not found: {}."
-        print(outs.format(args[0], oops.strerror))
-        sys.exit(1)
-
-
-def main(argv):
-    """Entry point for this script.
-
-    :param argv: command line arguments
-    """
-    checkfor(['gs', '-v'])
-    checkfor(['identify', '-version'])
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-w', '--width',
-                        default=160,
-                        type=float,
-                        help='width of the text block in mm.')
-    parser.add_argument('-v', '--version',
-                        action='version',
-                        version=__version__)
-    parser.add_argument('file', nargs='*')
-    args = parser.parse_args(argv)
-    args.width *= 72 / 25.4  # convert to points
-    del args.file[0]
-    if not args.file:
-        parser.print_help()
-        sys.exit(0)
-    for filename in args.file:
-        if not os.path.exists(filename):
-            print('File "{}" does not exist.'.format(filename))
-            continue
-        if filename.endswith(('.ps', '.PS', '.eps', '.EPS', '.pdf', '.PDF')):
-            bbox = getpdfbb(filename)
-            bbwidth = float(bbox[2]) - float(bbox[0])
-            scale = 1.0
-            if bbwidth > args.width:
-                scale = args.width / bbwidth
-            fs = '[viewport={} {} {} {},clip,scale={s:.3f}]'
-            opts = fs.format(*bbox, s=scale)
-        elif filename.endswith(('.png', '.PNG', '.jpg', '.JPG', '.jpeg',
-                                '.JPEG')):
-            width = getpixwidth(filename)
-            opts = ''
-            if width > args.width:
-                opts = '[scale={:.3f}]'.format(args.width / width)
-        else:
-            fskip = 'File "{}" has an unrecognized format. Skipping...'
-            print(fskip.format(filename))
-            continue
-        output_figure(filename, opts)
-    print()
 
 
 if __name__ == '__main__':
