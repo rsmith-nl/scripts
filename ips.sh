159cb41ab2aa2e0df1263c8a9f87191f0d4e413a
Roland Smith <rsmith@xs4all.nl>
2015-06-02 18:06:09 +0200

    Removed ips.sh
    The ips command has been implemented as a shell alias.

--- ips.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# file: ips.sh
-# vim:fileencoding=utf-8:ft=sh
-# Start a new terminal running IPython.
-#
-# Author: R.F. Smith <rsmith@xs4all.nl>
-# Created: 2015-05-02 09:27:08 +0200
-
-urxvt -title IPython -e ipython &
