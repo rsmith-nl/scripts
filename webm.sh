a89b9a3fe5d4162275c058c4efa4e14ea306b388
Roland Smith <rsmith@xs4all.nl>
2015-08-16 12:43:33 +0200

    Use vorbis audio in webm.sh.

--- webm.sh
+++ webm.sh
@@ -5,13 +5,14 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-06-21 16:44:34 +0200
-# Last modified: 2015-08-16 02:41:14 +0200
+# Last modified: 2015-08-16 12:39:11 +0200
 
 if [ $# -lt 2 ]; then
-    echo "Usage: webm <vod|crq|bq> file";
+    echo "Usage: webm <vod|crq|con|bq> file";
     echo "where;";
     echo "- vod means video on demand,";
-    echo "- crq means constrained rate quality and";
+    echo "- crq means constrained rate quality,";
+    echo "- con means constant rate quality and";
     echo "- bq means best quality.";
     echo "Constrained quality gives small file size.";
     exit 1;
@@ -23,16 +24,20 @@ FF=ffmpeg
 
 # Settings from:
 # https://sites.google.com/a/webmproject.org/wiki/ffmpeg/vp9-encoding-guide
+# Maar met Vorbis audio; dat klinkt beter.
 case $1 in
     vod) echo "Encoding for video on demand.";
         FP="-c:v libvpx-vp9 -pass 1 -b:v 1000K -threads 8 -speed 4 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -f webm -y /dev/null";
-         SP="-c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 8 -speed 1 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -y";;
+         SP="-c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 8 -speed 1 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -q:a 3 -f webm -y";;
     crq) echo "Encoding for constrained quality.";
          FP="-c:v libvpx-vp9 -pass 1 -b:v 1400k -crf 33 -threads 8 -speed 4 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -f webm -y /dev/null";
-        SP="-c:v libvpx-vp9 -pass 2 -b:v 1400k -crf 33 -threads 8 -speed 2 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -y";;
+        SP="-c:v libvpx-vp9 -pass 2 -b:v 1400k -crf 33 -threads 8 -speed 2 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -q:a 3 -f webm -y";;
+    con) echo "Encoding for constant quality.";
+         FP="-c:v libvpx-vp9 -pass 1 -b:v 0 -crf 20 -threads 8 -speed 4 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -f webm -y /dev/null";
+        SP="-c:v libvpx-vp9 -pass 2 -b:v 0 -crf 20 -threads 8 -speed 2 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -q:a 3 -f webm -y";;
     bq) echo "Encoding for best quality.";
          FP="-c:v libvpx-vp9 -pass 1 -b:v 1000K -threads 1 -speed 4 -tile-columns 0 -frame-parallel 0 -auto-alt-ref 1 -lag-in-frames 25 -g 9999 -aq-mode 0 -an -f webm -y /dev/null";
-        SP="-c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 1 -speed 0 -tile-columns 0 -frame-parallel 0 -auto-alt-ref 1 -lag-in-frames 25 -g 9999 -aq-mode 0 -c:a libopus -b:a 64k -f webm -y";;
+        SP="-c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 1 -speed 0 -tile-columns 0 -frame-parallel 0 -auto-alt-ref 1 -lag-in-frames 25 -g 9999 -aq-mode 0 -c:a libvorbis -q:a 3 -f webm -y";;
     *) echo "Unknown command"; exit 1;;
 esac
 
