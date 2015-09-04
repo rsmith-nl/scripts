c243199c3d4c12f3be785084e8c2113c39258de9
Roland Smith <rsmith@xs4all.nl>
2015-09-05 00:48:10 +0200

    Format output, handle bz2 files in denylog.py.

--- denylog.py
+++ denylog.py
@@ -3,16 +3,17 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-09-03 03:04:01 +0200
-# Last modified: 2015-09-03 08:47:08 +0200
+# Last modified: 2015-09-05 00:46:50 +0200
 
 """Summarize the deny log messages from ipfw in /var/log/security"""
 
 import argparse
+import bz2
 import logging
-import sys
 import re
+import sys
 
-__version__ = '0.0.1'
+__version__ = '0.3.0'
 
 
 def parselogfile(filename):
@@ -22,12 +23,16 @@ def parselogfile(filename):
         filename: Name of the file to read.
 
     Returns:
-        @todo
+        A tuple of (rule, IP, port) tuples
     """
-    with open(filename) as df:
-        data = df.read()
+    if filename.endswith('.bz2'):
+        df = bz2.open(filename, 'rt')
+    else:
+        df = open(filename)
+    data = df.read()
+    df.close()
     patt = 'ipfw: (\d+) Deny (?:TCP|UDP) ' \
-           '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d+)'
+           '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d+).*in'
     return tuple(set(re.findall(patt, data)))
 
 
@@ -50,14 +55,21 @@ def main(argv):
     args = parser.parse_args(argv)
     logging.basicConfig(level=getattr(logging, args.log.upper(), None),
                         format='%(levelname)s: %(message)s')
-    # The real program starts here...
     if not args.files:
         args.files = ['/var/log/security']
+    reps = '  IP: {:16s} port: {:6s} rule: {}'
     for f in args.files:
         print('File:', f)
-        matches = parselogfile(f)
+        try:
+            matches = parselogfile(f)
+        except FileNotFoundError as e:
+            print(e)
+            continue
+        if not matches:
+            print('  Nothing to report.')
+            continue
         for rule, IP, port in matches:
-            print('  IP: {}, port: {}, rule: {}'.format(IP, port, rule))
+            print(reps.format(IP+',', port+',', rule))
 
 
 if __name__ == '__main__':
