e03be2bd5c6c86247984da8aff99d1419f201c72
Roland Smith <rsmith@xs4all.nl>
2015-10-06 21:33:28 +0200

    Implement cmd-line options for vid2mp4

--- vid2mp4.py
+++ vid2mp4.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2015-10-05 22:28:08 +0200
+# Last modified: 2015-10-06 21:32:19 +0200
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to vid2mp4.py. This work is published from the
@@ -11,10 +11,12 @@
 """Convert all video files given on the command line to H.264/AAC streams in
 an MP4 container."""
 
-__version__ = '1.1.2'
+__version__ = '1.2.0'
 
+from functools import partial
 from multiprocessing import cpu_count
 from time import sleep
+import argparse
 import logging
 import os
 import subprocess
@@ -26,22 +28,37 @@ def main(argv):
     Entry point for vid2mp4.
 
     Arguments:
-        argv: All command line arguments.
+        argv: Command line arguments.
     """
-    if len(argv) == 1:
-        binary = os.path.basename(argv[0])
-        print("{} version {}".format(binary, __version__), file=sys.stderr)
-        print("Usage: {} [file ...]".format(binary), file=sys.stderr)
-        sys.exit(0)
-    logging.basicConfig(level="WARNING", format='%(levelname)s: %(message)s')
+    parser = argparse.ArgumentParser(description=__doc__)
+    crfh = 'constant rate factor (lower is better, default 29)'
+    parser.add_argument('-c', '--crf', type=int, default=29, help=crfh)
+    parser.add_argument('-p', '--preset', default='medium',
+                        choices=['ultrafast', 'superfast', 'veryfast',
+                                 'faster', 'fast', 'medium', 'slow', 'slower',
+                                 'veryslow'],
+                        help='preset (default medium) slower is smaller file')
+    parser.add_argument('--log', default='warning',
+                        choices=['debug', 'info', 'warning', 'error'],
+                        help="logging level (defaults to 'warning')")
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=__version__)
+    parser.add_argument("files", metavar='file', nargs='*',
+                        help="one or more files to process")
+    args = parser.parse_args(argv)
+    logging.basicConfig(level=getattr(logging, args.log.upper(), None),
+                        format='%(levelname)s: %(message)s')
+    logging.info('command line arguments = {}'.format(argv))
+    logging.info('parsed arguments = {}'.format(args))
     checkfor(['ffmpeg', '-version'])
-    vids = argv[1:]
+    starter = partial(startencoder, crf=args.crf, preset=args.preset)
     procs = []
     maxprocs = cpu_count()
-    for ifile in vids:
+    for ifile in args.files:
         while len(procs) == maxprocs:
             manageprocs(procs)
-        procs.append(startencoder(ifile))
+        procs.append(starter(ifile))
     while len(procs) > 0:
         manageprocs(procs)
 
@@ -70,13 +87,15 @@ def checkfor(args, rv=0):
         sys.exit(1)
 
 
-def startencoder(fname):
+def startencoder(fname, crf, preset):
     """
     Use ffmpeg to convert a video file to H.264/AAC streams in an MP4
     container.
 
     Arguments:
         fname: Name of the file to convert.
+        crf: Constant rate factor. See ffmpeg docs.
+        preset: Encoding preset. See ffmpeg docs.
 
     Returns:
         A 3-tuple of a Process, input path and output path.
@@ -89,8 +108,9 @@ def startencoder(fname):
         logging.warning(ls)
         return (None, fname, None)
     ofn = basename + '.mp4'
-    args = ['ffmpeg', '-i', fname, '-c:v', 'libx264', '-crf', '29', '-flags',
-            '+aic+mv4', '-c:a', 'libfaac', '-sn', ofn]
+    args = ['ffmpeg', '-i', fname, '-c:v', 'libx264', '-crf', str(crf),
+            '-preset', preset, '-flags',  '+aic+mv4', '-c:a', 'libfaac',
+            '-sn', '-y', ofn]
     try:
         p = subprocess.Popen(args, stdout=subprocess.DEVNULL,
                              stderr=subprocess.DEVNULL)
@@ -123,4 +143,4 @@ def manageprocs(proclist):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
