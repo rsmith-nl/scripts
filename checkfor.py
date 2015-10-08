f995089740ba89f3b9928349c2e18525e7a1fa14
Roland Smith <rsmith@xs4all.nl>
2015-10-08 22:10:19 +0200

    Improve consistency in checkfor

--- checkfor.py
+++ checkfor.py
@@ -1,14 +1,12 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2015-10-08 20:13:09 +0200
+# Last modified: 2015-10-08 22:09:41 +0200
 #
 # To the extent possible under law, Roland Smith has waived all
 # copyright and related or neighboring rights to checkfor.py This work
 # is published from the Netherlands. See
 # http://creativecommons.org/publicdomain/zero/1.0/
-
-from __future__ import division, print_function
 
 import logging
 import os
@@ -37,6 +35,6 @@ def checkfor(args, rv=0):
             raise OSError
         logging.info('found required program "{}"'.format(args[0]))
     except OSError as oops:
-        outs = "required program '{}' not found: {}."
+        outs = 'required program "{}" not found: {}.'
         logging.error(outs.format(args[0], oops.strerror))
         sys.exit(1)
