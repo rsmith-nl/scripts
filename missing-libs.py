40c3d38859610fc3889cb62fb70390783d66ed25
Roland Smith <rsmith@xs4all.nl>
2016-01-17 15:48:11 +0100

    Added missing-libs.py.

---
+++ missing-libs.py
@@ -0,0 +1,85 @@
+# file: missing-libs.py
+# vim:fileencoding=utf-8:ft=python
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2016-01-17 15:00:08 +0100
+# Last modified: 2016-01-17 15:47:41 +0100
+
+"""Check if any of the files in the given directory are executables linked to
+missing shared libraries."""
+
+import argparse
+import logging
+import sys
+import os
+import subprocess as sp
+from enum import Enum
+
+__version__ = '0.0.1'
+
+
+class Ftype(Enum):
+    script = 1
+    executable = 2
+    other = 3
+    unaccessible = 4
+
+
+def main(argv):
+    """
+    Entry point for missing-libs.py.
+
+    Arguments:
+        argv: command line arguments
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=__version__)
+    parser.add_argument('--log', default='warning',
+                        choices=['debug', 'info', 'warning', 'error'],
+                        help="logging level (defaults to 'warning')")
+    parser.add_argument("dirs", nargs='*',
+                        help="one or more directory to process")
+    args = parser.parse_args(argv)
+    logging.basicConfig(level=getattr(logging, args.log.upper(), None),
+                        format='%(levelname)s: %(message)s')
+    logging.debug('Command line arguments = {}'.format(argv))
+    logging.debug('Parsed arguments = {}'.format(args))
+    for d in args.dirs:
+        logging.info('examining files in {}'.format(d))
+        data = [get_type(e.path) for e in os.scandir(d)]
+        exe = [a for a, b in data if b == Ftype.executable]
+        for e in exe:
+            check_missing_libs(e)
+
+
+def get_type(path):
+    """Returns a tuple (path, Ftype)"""
+    try:
+        with open(path, 'rb') as p:
+            data = p.read(2)
+    except OSError:
+        logging.warning("cannot access {}".format(path))
+        return (path, Ftype.unaccessible)
+    if data == b'#!':
+        return (path, Ftype.script)
+    elif data == b'\x7f\x45':
+        return (path, Ftype.executable)
+    else:
+        return (path, Ftype.other)
+
+
+def check_missing_libs(path):
+    logging.info("checking {}".format(path))
+    try:
+        out = sp.check_output(['ldd', path]).decode('utf-8').splitlines()
+        for ln in out:
+            if 'missing' in ln:
+                print(path, ln)
+    except sp.CalledProcessError:
+        logging.info('ldd failed on {}'.format(path))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
