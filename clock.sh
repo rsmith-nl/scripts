d1f082b4b16fafd20ce48c119b8261c22aec5289
Roland Smith <rsmith@xs4all.nl>
2016-02-29 19:08:01 +0100

    Change clock zones.

--- clock.sh
+++ clock.sh
@@ -2,13 +2,13 @@
 #
 # Script to show all the times that are important for me.
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2016-01-10 21:24:11 +0100
+# Last modified: 2016-02-29 19:07:00 +0100
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to 'clock'. This work is published from the
 # Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
 
-ZONES="UTC Europe/Amsterdam EST Asia/Shanghai Asia/Seoul Pacific/Auckland"
+ZONES="UTC Europe/Amsterdam Europe/London Asia/Shanghai Asia/Seoul Pacific/Auckland"
 
 for z in $ZONES; do
     printf "%16s: " $z
