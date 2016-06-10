29afefbb9e39299d1903d840be9cd6c17eee3ea0
Roland Smith <rsmith@xs4all.nl>
2016-06-11 00:31:31 +0200

    Add extra tests.

--- scripts-tests.py
+++ scripts-tests.py
@@ -3,7 +3,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-04-06 13:08:02 +0200
-# Last modified: 2016-06-10 20:49:02 +0200
+# Last modified: 2016-06-11 00:30:24 +0200
 
 """
 Tests for functions in python files in the scripts directory.
@@ -11,8 +11,29 @@ Tests for functions in python files in the scripts directory.
 To run the tests use: py.test -v scripts-tests.py
 """
 
+from collections import Counter
+
+from genotp import rndcaps, otp
 from genpw import roundup, genpw
 from nospaces import fixname
+from offsetsrt import str2ms, ms2str
+
+
+def test_rndcaps():
+    rv = rndcaps(20)
+    assert len(rv) == 20
+    assert all(j in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for j in rv)
+    v = Counter(rndcaps(100000)).values()
+    mxc = max(v)
+    mnc = min(v)
+    assert mxc/mnc < 1.3
+
+
+def test_otp():
+    lines = otp(5).splitlines()
+    assert len(lines) == 5
+    for ln in lines:
+        assert len(ln) == 75
 
 
 def test_roundup():
@@ -40,3 +61,11 @@ def test_genpw():
 def test_fixname():
     rv = fixname('dit is  een\ttest')
     assert rv == 'dit_is_een_test'
+
+
+def test_srt():
+    for j in range(7200):
+        p = j*10
+        ts = ms2str(p)
+        k = str2ms(ts)
+        assert p == k
