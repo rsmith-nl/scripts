155cfc60c2ad517df733debd4683d2716b4a3e0e
Roland Smith <rsmith@xs4all.nl>
2016-08-09 00:21:38 +0200

    Find cropping information if not given.

--- dvd2webm.py
+++ dvd2webm.py
@@ -4,7 +4,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2016-02-10 22:42:09 +0100
-# Last modified: 2016-08-07 12:29:01 +0200
+# Last modified: 2016-08-08 15:40:07 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to dvd2webm.py. This work is published
@@ -12,16 +12,18 @@
 
 """Convert an mpeg stream from a DVD to a webm file."""
 
+from collections import Counter
 from datetime import datetime
 import argparse
 import json
 import logging
 import math
 import os
+import re
 import subprocess as sp
 import sys
 
-__version__ = '0.5.2'
+__version__ = '0.7.0'
 
 
 def main(argv):
@@ -39,18 +41,57 @@ def main(argv):
                         help="crop (w:h:x:y) to use")
     parser.add_argument('-t', '--subtitle', type=str,
                         help="name of srt file for subtitles")
+    ahelp = "number of the audio track to use (default: 0; first audio track)"
+    parser.add_argument('-a', '--audio', type=int, default=0,
+                        help=ahelp)
     parser.add_argument('fn', metavar='filename', help='MPEG file to process')
     args = parser.parse_args(argv)
     logging.basicConfig(level=getattr(logging, args.log.upper(), None),
                         format='%(levelname)s: %(message)s')
     logging.debug('command line arguments = {}'.format(argv))
     logging.debug('parsed arguments = {}'.format(args))
-    logging.info("processing '{}'".format(args.fn))
-    logging.info("started at " + str(datetime.now())[:-7])
+    logging.info("processing '{}'.".format(args.fn))
+    logging.info('started at {}.'.format(str(datetime.now())[:-7]))
     vdata = vidinfo(args.fn)
+    if not args.crop:
+        logging.info('looking for cropping.')
+        args.crop = findcrop(args.fn)
+        width, height, _, _ = args.crop.split(':')
+        if width in ['720', '704'] and height == '576':
+            logging.info('standard format, no cropping necessary.')
+            args.crop = None
     a1, a2 = mkargs(args.fn, args.crop, args.start, args.subtitle,
-                    vdata["width"])
+                    vdata["width"], args.audio)
     encode(a1, a2)
+
+
+def findcrop(path, start='00:10:00', duration='00:00:01'):
+    """
+    Find the cropping of the video file.
+
+    Arguments:
+        path: location of the file to query.
+        start: A string that defines where in the movie to start scanning.
+            Defaults to 10 minutes from the start. Format HH:MM:SS.
+        duration: A string defining how much of the movie to scan. Defaults to
+            one second. Format HH:MM:SS.
+
+    Returns:
+        A string containing the cropping to use with ffmpeg.
+    """
+    args = ['ffmpeg', '-hide_banner',
+            '-ss', start,  # Start at 10 minutes in.
+            '-t', duration,  # Parse for one second.
+            '-i', path,  # Path to the input file.
+            '-vf', 'cropdetect',  # Use the crop detect filter.
+            '-an',  # Disable audio output.
+            '-y',  # Overwrite output without asking.
+            '-f', 'rawvideo',  # write raw video output.
+            '/dev/null'  # Write output to /dev/null
+            ]
+    proc = sp.run(args, universal_newlines=True, stderr=sp.PIPE)
+    rv = Counter(re.findall('crop=(\d+:\d+:\d+:\d+)', proc.stderr))
+    return rv.most_common(1)[0][0]
 
 
 def vidinfo(path):
@@ -66,8 +107,8 @@ def vidinfo(path):
     """
     args = ['ffprobe', '-hide_banner', '-show_streams', '-select_streams',
             'v:0', '-of', 'json', path]
-    rv = sp.check_output(args, stderr=sp.DEVNULL, universal_newlines=True)
-    vdata = json.loads(rv)["streams"][0]
+    proc = sp.run(args, stdout=sp.PIPE, universal_newlines=True)
+    vdata = json.loads(proc.stdout)["streams"][0]
     return vdata
 
 
@@ -83,7 +124,7 @@ def reporttime(p, start, end):
     logging.info('pass {} took {}.'.format(p, dt[:-7]))
 
 
-def mkargs(fn, crop, start, subfname, width):
+def mkargs(fn, crop, start, subfname, width, atrack):
     """
     Create argument lists for two-pass constrained rate VP9/Vorbis encoding
     in a webm container.
@@ -94,6 +135,7 @@ def mkargs(fn, crop, start, subfname, width):
         start: String telling ffmeg the time when to start encoding.
         subfname: Path of the SRT subtitles file.
         width: Width of the video stream in pixels.
+        atrack: number of the audio track (0-based indexing)
 
     Returns:
         A tuple (args, args2) which are the argument lists for starting
@@ -101,32 +143,32 @@ def mkargs(fn, crop, start, subfname, width):
     """
     basename = fn.rsplit('.', 1)[0]
     numcolumns = str(int(math.log2(width/256)))
-    logging.info('using tile-columns flag set to ' + numcolumns)
+    logging.info('using tile-columns flag set to ' + numcolumns + '.')
     numthreads = str(os.cpu_count() - 1)
-    logging.info('using {} threads'.format(numthreads))
+    logging.info('using {} threads.'.format(numthreads))
     args = ['ffmpeg', '-loglevel', 'quiet', '-i', fn, '-passlogfile', basename,
             '-c:v', 'libvpx-vp9', '-threads', numthreads, '-pass', '1', '-sn',
             '-b:v', '1400k', '-crf', '33', '-g', '250', '-speed', '4',
             '-tile-columns', numcolumns, '-an', '-f', 'webm',
-            '-map', 'i:0x1e0', '-map', 'i:0x80', '-y', '/dev/null']
+            '-map', '0:v', '-map', '0:a:{}'.format(atrack), '-y', '/dev/null']
     args2 = ['ffmpeg', '-loglevel', 'quiet', '-i', fn,
              '-passlogfile', basename, '-c:v', 'libvpx-vp9',
              '-threads', numthreads, '-pass', '2', '-sn', '-b:v', '1400k',
              '-crf', '33', '-g', '250', '-speed', '2',
              '-tile-columns', numcolumns, '-auto-alt-ref', '1',
              '-lag-in-frames', '25', '-c:a', 'libvorbis', '-q:a', '3',
-             '-f', 'webm', '-map', 'i:0x1e0', '-map', 'i:0x80',
+             '-f', 'webm', '-map', '0:v', '-map', '0:a:{}'.format(atrack),
              '-y', '{}.webm'.format(basename)]
     vf = []
     if subfname:
-        logging.info("using subtitles from '{}'".format(subfname))
+        logging.info("using subtitles from '{}'.".format(subfname))
         vf.append('subtitles={}'.format(subfname))
     if crop:
-        logging.info('using cropping {}'.format(crop))
+        logging.info('using cropping {}.'.format(crop))
         vf.append('crop={}'.format(crop))
     if vf:
         opts = ','.join(vf)
-        logging.debug('vf options: {}'.format(opts))
+        logging.debug('vf options: {}.'.format(opts))
         args.insert(-2, '-vf')
         args2.insert(-2, '-vf')
         args.insert(-2, opts)
@@ -154,7 +196,7 @@ def encode(args1, args2):
     proc = sp.run(args1, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
     end = datetime.utcnow()
     if proc.returncode:
-        logging.error('pass 1 returned {}'.format(proc.returncode))
+        logging.error('pass 1 returned {}.'.format(proc.returncode))
         return
     else:
         reporttime(1, start, end)
@@ -163,13 +205,13 @@ def encode(args1, args2):
     proc = sp.run(args2, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
     end = datetime.utcnow()
     if proc.returncode:
-        logging.error('pass 2 returned {}'.format(proc.returncode))
+        logging.error('pass 2 returned {}.'.format(proc.returncode))
     else:
         reporttime(2, start, end)
     origsize = os.path.getsize(args2[4])
     newsize = os.path.getsize(args2[-1])
     percentage = int(100*newsize/origsize)
-    sz = "the size of '{}' is {}% of the size of '{}'"
+    sz = "the size of '{}' is {}% of the size of '{}'."
     logging.info(sz.format(args2[-1], percentage, args2[4]))
 
 
