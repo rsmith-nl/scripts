edbc1956f595447497aa77fba02e9fe672bc52a8
Roland Smith <rsmith@xs4all.nl>
2017-05-20 22:20:07 +0200

    Hardcode substream IDs in dvd2webm.

--- dvd2webm.py
+++ dvd2webm.py
@@ -4,19 +4,18 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2016-02-10 22:42:09 +0100
-# Last modified: 2017-04-30 17:37:08 +0200
+# Last modified: 2017-05-20 22:19:26 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to dvd2webm.py. This work is published
 # from the Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
 
 """Convert an mpeg stream from a DVD to a webm file, using the main video stream
-and the main AC-3 audio stream (substream ID 0x80)."""
+(substream ID 0x1e0) and the main AC-3 audio stream (substream ID 0x80)."""
 
 from collections import Counter
 from datetime import datetime
 import argparse
-import json
 import logging
 import math
 import os
@@ -24,7 +23,7 @@ import re
 import subprocess as sp
 import sys
 
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 
 
 def main(argv):
@@ -42,9 +41,6 @@ def main(argv):
                         help="crop (w:h:x:y) to use")
     parser.add_argument('-t', '--subtitle', type=str,
                         help="name of srt file for subtitles")
-    ahelp = "number of the audio track to use (default: 0; first audio track)"
-    parser.add_argument('-a', '--audio', type=int, default=0,
-                        help=ahelp)
     parser.add_argument('fn', metavar='filename', help='MPEG file to process')
     args = parser.parse_args(argv)
     logging.basicConfig(level=getattr(logging, args.log.upper(), None),
@@ -53,20 +49,6 @@ def main(argv):
     logging.debug('parsed arguments = {}'.format(args))
     logging.info("processing '{}'.".format(args.fn))
     logging.info('started at {}.'.format(str(datetime.now())[:-7]))
-    si = streaminfo(args.fn)
-    sk = si.keys()
-    if "0x1e0" not in sk:
-        logging.error('video stream 0x1e0 not found!')
-        exit(1)
-    elif "width" not in si["0x1e0"].keys():
-        logging.error('video stream 0x1e0 has no width')
-        exit(1)
-    if "0x80" not in sk:
-        logging.error('english audio stream (0x80) not found!')
-        exit(2)
-    logging.info('video stream {}.'.format(si["0x1e0"]))
-    logging.info('audio stream {}.'.format(si["0x80"]))
-    args.audio = si["0x80"]["index"]
     if not args.crop:
         logging.info('looking for cropping.')
         args.crop = findcrop(args.fn)
@@ -74,8 +56,7 @@ def main(argv):
         if width in ['720', '704'] and height == '576':
             logging.info('standard format, no cropping necessary.')
             args.crop = None
-    a1, a2 = mkargs(args.fn, args.crop, args.start, args.subtitle,
-                    si["0x1e0"]["width"], args.audio)
+    a1, a2 = mkargs(args.fn, args.crop, args.start, args.subtitle)
     encode(a1, a2)
 
 
@@ -109,25 +90,6 @@ def findcrop(path, start='00:10:00', duration='00:00:01'):
     return rv.most_common(1)[0][0]
 
 
-def streaminfo(path):
-    """Retrieve the stream index and id and the width of video streams.
-
-    Argument:
-        path: location of the file to query.
-
-    Returns:
-        A dict of dicts. Each containing "index", "id" and possibly "width".
-        The outer dict is keyed by the stream ID.
-    """
-    args = ['ffprobe', '-hide_banner', '-show_entries', 'stream=index,id,width',
-            '-of', 'json', path]
-    proc = sp.run(args, stdout=sp.PIPE, stderr=sp.DEVNULL,
-                  universal_newlines=True)
-    streamdata = json.loads(proc.stdout)["streams"]
-    rv = {i["id"]: i for i in streamdata}
-    return rv
-
-
 def reporttime(p, start, end):
     """
     Report the amount of time passed between start and end.
@@ -140,7 +102,7 @@ def reporttime(p, start, end):
     logging.info('pass {} took {}.'.format(p, dt[:-7]))
 
 
-def mkargs(fn, crop, start, subfname, width, atrack):
+def mkargs(fn, crop, start, subfname, width):
     """
     Create argument lists for two-pass constrained rate VP9/Vorbis encoding
     in a webm container.
@@ -151,7 +113,6 @@ def mkargs(fn, crop, start, subfname, width, atrack):
         start: String telling ffmeg the time when to start encoding.
         subfname: Path of the SRT subtitles file.
         width: Width of the video stream in pixels.
-        atrack: number of the audio track (0-based indexing)
 
     Returns:
         A tuple (args1, args2) which are the argument lists for starting
@@ -169,14 +130,14 @@ def mkargs(fn, crop, start, subfname, width, atrack):
              '-c:v', 'libvpx-vp9', '-threads', numthreads, '-pass', '1', '-sn',
              '-b:v', '1400k', '-crf', '33', '-g', '250', '-speed', '4',
              '-tile-columns', numcolumns, '-an', '-f', 'webm',
-             '-map', '0:v', '-map', '0:{}'.format(atrack), '-y', '/dev/null']
+             '-map', '#0x1e0', '-map', '#0x80', '-y', '/dev/null']
     args2 = ['ffmpeg', '-loglevel', 'quiet', '-i', fn,
              '-passlogfile', basename, '-c:v', 'libvpx-vp9',
              '-threads', numthreads, '-pass', '2', '-sn', '-b:v', '1400k',
              '-crf', '33', '-g', '250', '-speed', '2',
              '-tile-columns', numcolumns, '-auto-alt-ref', '1',
              '-lag-in-frames', '25', '-c:a', 'libvorbis', '-q:a', '3',
-             '-f', 'webm', '-map', '0:v', '-map', '0:{}'.format(atrack),
+             '-f', 'webm', '-map', '#0x1e0', '-map', '#0x80',
              '-y', '{}.webm'.format(basename)]
     vf = []
     if subfname:
