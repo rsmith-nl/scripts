d1b1b6e325345fc62b98d6402cfd284fb24698be
Roland Smith <rsmith@xs4all.nl>
2017-12-09 09:52:08 +0100

    Add JPEG compression.

--- tifftopdf.py
+++ tifftopdf.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2017-12-09 09:27:25 +0100
+# Last modified: 2017-12-09 09:51:44 +0100
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to tiff2pdf.py. This work is published from
@@ -13,6 +13,7 @@ Convert TIFF files to PDF format.
 Using the utilities tiffinfo and tiff2pdf from the libtiff package.
 """
 
+from functools import partial
 import argparse
 import concurrent.futures as cf
 import logging
@@ -21,7 +22,7 @@ import re
 import subprocess
 import sys
 
-__version__ = '1.3'
+__version__ = '1.4'
 
 
 def main(argv):
@@ -38,6 +39,11 @@ def main(argv):
         choices=['debug', 'info', 'warning', 'error'],
         help="logging level (defaults to 'warning')")
     parser.add_argument(
+        '-j', '--jpeg', help='use JPEG compresion', action="store_true")
+    parser.add_argument(
+        '-q', '--quality', help='JPEG compresion quality (default 85)',
+        type=int, default=85)
+    parser.add_argument(
         '-v', '--version', action='version', version=__version__)
     parser.add_argument(
         "files",
@@ -53,8 +59,12 @@ def main(argv):
     checkfor('tiffinfo', 255)
     checkfor(['tiff2pdf', '-v'])
     errmsg = 'conversion of {} failed, return code {}'
+    func = tiffconv
+    if args.jpeg:
+        logging.info('using JPEG compression.')
+        func = partial(tiffconv, jpeg=True, quality=args.quality)
     with cf.ThreadPoolExecutor(max_workers=os.cpu_count()) as tp:
-        for fn, rv in tp.map(tiffconv, args.files):
+        for fn, rv in tp.map(func, args.files):
             if rv == 0:
                 logging.info('finished "{}"'.format(fn))
             else:
@@ -88,12 +98,14 @@ def checkfor(args, rv=0):
         sys.exit(1)
 
 
-def tiffconv(fname):
+def tiffconv(fname, jpeg=False, quality=85):
     """
     Start a tiff2pdf process for given file.
 
     Arguments:
         name: Name of the tiff file to convert.
+        jpeg: Use JPEG compression.
+        quality: JPEG compression quality.
 
     Returns:
         A 2-tuple (input filename, tiff2pdf return value).
@@ -114,9 +126,9 @@ def tiffconv(fname):
         except ValueError:
             xres, yres = None, None
         outname = re.sub('\.tif{1,2}?$', '.pdf', fname, flags=re.IGNORECASE)
+        program = ['tiff2pdf']
         if xres:
             args = [
-                'tiff2pdf',
                 '-w', str(width / xres),
                 '-l', str(length / xres),
                 '-x', str(xres), '-y', str(yres),
@@ -124,13 +136,18 @@ def tiffconv(fname):
             ]
         else:
             args = [
-                'tiff2pdf', '-o', outname,
+                '-o', outname,
                 '-z', '-p', 'A4', '-F', fname]
             ls = "no resolution in {}. Fitting to A4"
             logging.warning(ls.format(fname))
+        if jpeg:
+            args = program + ['-n', '-j', '-q', str(quality)] + args
+        else:
+            args = program + args
+        logging.info('calling "{}"'.format(' '.join(args)))
         rv = subprocess.call(
             args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        logging.info('finished "{}"'.format(outname))
+        logging.info('created "{}"'.format(outname))
         return (fname, rv)
     except Exception as e:
         ls = 'starting conversion of "{}" failed: {}'
