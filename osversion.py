fa66b4d4172f0134dbc0c68a279e23c9b20d9132
Roland Smith <rsmith@xs4all.nl>
2018-07-06 22:55:19 +0200

    Omschrijving aangepast.

--- osversion.py
+++ osversion.py
@@ -4,14 +4,14 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2018-04-06 22:34:00 +0200
-# Last modified: 2018-04-06 22:39:44 +0200
+# Last modified: 2018-07-06T22:54:44+0200
 
 from ctypes import CDLL
 
 with open('/usr/include/osreldate.h') as h:
     lines = h.readlines()
 line = [ln for ln in lines if ln.startswith('#define')][0]
-print('Compilation environment release date:', line.split()[-1])
+print('Compilation environment version:', line.split()[-1])
 
 libc = CDLL("/lib/libc.so.7")
-print('Execution environment release date:', libc.getosreldate())
+print('Execution environment version:', libc.getosreldate())
