8b3ad1ea09c1fd8f2d433e79497eb51d9f32f4c7
Roland Smith <rsmith@xs4all.nl>
2018-08-19 14:23:43 +0200

    Print a warning and exit when not run on FreeBSD.

--- osversion.py
+++ osversion.py
@@ -4,9 +4,15 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2018-04-06 22:34:00 +0200
-# Last modified: 2018-07-06T22:54:44+0200
+# Last modified: 2018-08-19T14:18:16+0200
+"""Print the __FreeBSD_version. This is also called OSVERSION in scripts."""
 
 from ctypes import CDLL
+import sys
+
+if 'freebsd' not in sys.platform:
+    print('This script only works on FreeBSD!')
+    sys.exit(1)
 
 with open('/usr/include/osreldate.h') as h:
     lines = h.readlines()
