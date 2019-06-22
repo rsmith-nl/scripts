10851bfa8b647ed14ffaf42262f46a6844718b48
Roland Smith <rsmith@xs4all.nl>
2019-06-22 19:44:08 +0200

    Generate the control message as an array.

--- set-ornata-chroma-rgb.py
+++ set-ornata-chroma-rgb.py
@@ -5,23 +5,24 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2019-06-16T19:09:06+0200
-# Last modified: 2019-06-16T21:52:22+0200
+# Last modified: 2019-06-22T19:43:10+0200
 """Set the LEDs on a Razer Ornata Chroma keyboard to a static RGB color."""
 
+import array
 import argparse
 import logging
 import sys
 import usb.core
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 def static_color_msg(red, green, blue):
     """
     Create a message to set the Razer Ornata Croma lights to a static color.
-    All arguments should be convertable to an integer in the range 0-255.
+    All arguments should convert to an integer in the range 0-255.
 
-    Returns a bytearray containing the message.
+    Returns an array.array containing the message ready to feed into a ctrl_transfer.
     """
 
     def _chk(name, channel):
@@ -29,38 +30,29 @@ def static_color_msg(red, green, blue):
             isinstance(channel, str) and len(channel) > 2 and channel[0] == '0' and
             channel[1] in 'bBoOxX'
         ):
+            # Decode binary, octal and hexadecimal numbers.
             channel = int(channel, {'b': 2, 'o': 8, 'x': 16}[channel[1].lower()])
         else:
             channel = int(channel)
         if channel < 0 or channel > 255:
             logging.error(f'{name} value should be in the range 0 to 255')
+            # There is no point continuing with an invalid input
+            # *in this case* of a command-line script.
             sys.exit(2)
         return channel
+    red, green, blue = _chk('red', red), _chk('green', green), _chk('blue', blue)
 
-    red = _chk('red', red)
-    green = _chk('green', green)
-    blue = _chk('blue', blue)
-    msg = bytearray(90)
-    # byte 0 is 0
-    msg[1] = 0x3F  # transaction id
-    # bytes 2-4 are 0.
-    msg[5] = 0x09  # data size
-    msg[6] = 0x0F  # command class
-    msg[7] = 0x02  # command id
-    # The rest of the msg bytes are variable data
-    msg[8] = 0x01  # VARSTORE
-    msg[9] = 0x05  # BACKLIGHT_LED
-    msg[10] = 0x01  # effect id
-    # bytes 11-12 are 0.
-    msg[13] = 0x01
-    msg[14] = red
-    msg[15] = green
-    msg[16] = blue
-    # Calculate and set the checksum.
+    # Meaning of the nonzero bytes, in sequence:
+    # 0x3f = transaction id, 0x09 = variable data length, 0x0f = command class,
+    # 0x02 = command id, 0x01 = VARSTORE, 0x05 = BACKLIGHT_LED, 0x01 = effect id,
+    # 0x01 =  unknown
+    msg = array.array('B', b'\x00\x3f\x00\x00\x00\x09\x0f\x02\x01\x05\x01\x00\x00\x01')
+    msg.extend([red, green, blue])
     crc = 0
-    for j in msg[2:88]:
+    for j in msg[2:]:
         crc ^= j
-    msg[-2] = crc
+    msg.extend(bytes(71))
+    msg.extend([crc, 0])
     return msg
 
 
@@ -94,10 +86,12 @@ def main(argv):
     if dev is None:
         logging.error('No Razer Ornata Chroma keyboard found')
         sys.exit(1)
-    msg = static_color_msg(args.red, args.green, args.blue)  # set color to Green.
+    logging.debug(str(dev))
+    msg = static_color_msg(args.red, args.green, args.blue)
     logging.info(f'red={args.red}')
     logging.info(f'green={args.green}')
     logging.info(f'blue={args.blue}')
+    logging.debug(f'message={msg}')
     read = dev.ctrl_transfer(0x21, 0x09, 0x300, 0x01, msg)
     if read != 90:
         logging.error('control transfer for setting the color failed')
