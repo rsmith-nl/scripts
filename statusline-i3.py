7b735bc4ce678dc80f117f02b9acfaa5b37d3ad0
Roland Smith <rsmith@xs4all.nl>
2019-07-02 01:13:22 +0200

    Version 1 of statusline program.

--- statusline-i3.py
+++ statusline-i3.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-# file: i3-status.py
+# file: statusline-i3.py
 # vim:fileencoding=utf-8:fdm=marker:ft=python
 #
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-06-30T22:23:11+0200
-# Last modified: 2019-07-01T02:17:52+0200
+# Last modified: 2019-07-02T01:10:49+0200
 """Generate status line for i3 on my machine."""
 
 import os
 import statistics as stat
 import subprocess as sp
 import time
+
+__version__ = 1.0
 
 # Low level functions.
 
@@ -56,30 +58,21 @@ def network():
             items.append(f'{desc}: 0B/0B')
     return ' '.join(items)
 
-# In [12]: %timeit read = len(re.findall('^Status: R', data, flags=re.MULTILINE))
-# 381 ms ± 1.13 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
-#
-# In [13]: %timeit total = len(re.findall('^From ', data, flags=re.MULTILINE))
-# 381 ms ± 1.74 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
-#
-# In [17]: %timeit len([ln for ln in lines if ln.startswith('Status: R')])
-# 83.6 ms ± 72.6 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-
-# In [18]: %timeit len([ln for ln in lines if ln.startswith('From ')])
-# 83.9 ms ± 88.9 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-
 
 def mail():
     mboxname = '/home/rsmith/Mail/received'
-    global mailcount
-    if time.time() - os.stat(mboxname).st_mtime < 1 or mailcount < 0:
+    global lastmail
+    newtime = os.stat(mboxname).st_mtime
+    if newtime > lastmail['time']:
         with open(mboxname) as mbox:
             lines = mbox.readlines()
         # This is faster than regex; see above.
         read = len([ln for ln in lines if ln.startswith('Status: R')])
         total = len([ln for ln in lines if ln.startswith('From ')])
         mailcount = total - read
-        print("DEBUG: mailcount = ", mailcount)
+        lastmail = {'count': mailcount, 'time': newtime}
+    else:
+        mailcount = lastmail['count']
     return f'Mail: {mailcount}'
 
 
@@ -92,30 +85,23 @@ def memory():
 
 
 def cpu():
-    items = [f'dev.cpu.{n}.temperature' for n in range(4)]
+    global cpusage
+    items = [f'dev.cpu.{n}.temperature' for n in range(4)] + ['kern.cp_time']
     lines = sp.check_output(['sysctl'] + items, encoding='ascii').splitlines()
-    temps = [float(ln.split()[1][:-1]) for ln in lines]
+    temps = [float(ln.split()[1][:-1]) for ln in lines[:-1]]
     T = round(stat.mean(temps))
-
-    return f'CPU: %, {T}°C'
+    states = [int(j) for j in lines[-1].split()[1:]]
+    # According to /usr/include/sys/resource.h, these are:
+    # USER, NICE, SYS, INT, IDLE
+    total = sum(states)
+    used = total - states[-1]
+    frac = int((used - cpusage['used']) / (total - cpusage['total']) * 100)
+    cpusage = {'used': used, 'total': total}
+    return f'CPU: {frac}%, {T}°C'
 
 
 def date():
     return time.strftime('%a %Y-%m-%d %H:%M:%S')
-
-
-# Statistics:
-#In [5]: %timeit network()
-#156 ms ± 1.34 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
-#
-#In [6]: %timeit memory()
-#77.4 ms ± 118 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-#
-#In [7]: %timeit cpu()
-#76.3 ms ± 138 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-#
-#In [8]: %timeit date()
-#4.83 µs ± 12.8 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
 
 
 def main():
@@ -128,14 +114,15 @@ def main():
         print(' | '.join(items))
         end = time.time()
         delta = end - start
-        print("DEBUG: delta = ", delta)
+        # print(f"DEBUG: looptijd = {delta:.2f} s")
         if delta < 1:
             time.sleep(1 - delta)
 
 
 # Global data
 netdata = {'age0': (0, 0, 0), 'rl0': (0, 0, 0)}
-mailcount = -1
+lastmail = {'count': -1, 'time': 0}
+cpusage = {'used': 0, 'total': 0}
 
 if __name__ == '__main__':
     main()
