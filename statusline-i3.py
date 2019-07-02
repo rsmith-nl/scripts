3854202fca817a80d6f14982a3c2ccde52da5a9d
Roland Smith <rsmith@xs4all.nl>
2019-07-02 02:27:36 +0200

    Ombouwen naar gebruik van sysctls via ctypes.

--- statusline-i3.py
+++ statusline-i3.py
@@ -5,17 +5,75 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-06-30T22:23:11+0200
-# Last modified: 2019-07-02T01:10:49+0200
+# Last modified: 2019-07-02T02:28:36+0200
 """Generate status line for i3 on my machine."""
 
+import ctypes
 import os
 import statistics as stat
+import struct
 import subprocess as sp
 import time
 
-__version__ = 1.0
+__version__ = 1.99
 
 # Low level functions.
+
+
+def to_int(value):
+    """Convert binary sysctl value to integer."""
+    return int.from_bytes(value, byteorder='little')
+
+
+def to_degC(value):
+    """Convert binary sysctl value to degree Centigrade."""
+    return round(int.from_bytes(value, byteorder='little')/10 - 273.15, 1)
+
+
+def sysctlbyname(name, buflen=4, convert=None):
+    """
+    Python wrapper for sysctlbyname(3) on FreeBSD.
+
+    Arguments:
+        name (str): Name of the sysctl to query
+        buflen (int): Length of the data buffer to use.
+        convert: Optional function to convert the data.
+    """
+    name_in = ctypes.c_char_p(bytes(name, encoding='ascii'))
+    oldlen = ctypes.c_size_t(buflen)
+    oldlenp = ctypes.byref(oldlen)
+    oldp = ctypes.create_string_buffer(buflen)
+    rv = libc.sysctlbyname(name_in, oldp, oldlenp, None, ctypes.c_size_t(0))
+    if rv != 0:
+        raise ValueError(f'sysctlbyname error')
+    if convert:
+        return convert(oldp.raw[:buflen])
+    return oldp.raw[:buflen]
+
+
+def sysctl(name, buflen=4, convert=None):
+    """
+    Python wrapper for sysctl(3) on FreeBSD.
+
+    Arguments:
+        name: list or tuple of integers.
+        buflen (int): Length of the data buffer to use.
+        convert: Optional function to convert the data.
+    """
+    cnt = len(name)
+    mib = ctypes.c_int * cnt
+    name_in = mib(*name)
+    oldlen = ctypes.c_size_t(buflen)
+    oldlenp = ctypes.byref(oldlen)
+    oldp = ctypes.create_string_buffer(buflen)
+    rv = libc.sysctl(
+        name_in, ctypes.c_int(cnt), oldp, oldlenp, None, ctypes.c_size_t(0)
+    )
+    if rv != 0:
+        raise ValueError(f'sysctl error')
+    if convert:
+        return convert(oldp.raw[:buflen])
+    return oldp.raw[:buflen]
 
 
 def netstat(interface):
@@ -29,15 +87,6 @@ def netstat(interface):
     ibytes = int(items[7])
     obytes = int(items[10])
     return ibytes, obytes, tm
-
-
-def vmstats():
-    """Return the vm stats."""
-    rv = {}
-    for ln in sp.check_output(['sysctl', 'vm.stats.vm'], encoding='ascii').splitlines():
-        name, count = ln.split()
-        rv[name[14:-1]] = int(count)
-    return rv
 
 
 # High level functions
@@ -66,7 +115,7 @@ def mail():
     if newtime > lastmail['time']:
         with open(mboxname) as mbox:
             lines = mbox.readlines()
-        # This is faster than regex; see above.
+        # This is faster than regex.
         read = len([ln for ln in lines if ln.startswith('Status: R')])
         total = len([ln for ln in lines if ln.startswith('From ')])
         mailcount = total - read
@@ -77,7 +126,11 @@ def mail():
 
 
 def memory():
-    stats = vmstats()
+    suffixes = ('page_count', 'free_count', 'inactive_count', 'cache_count')
+    stats = {
+        suffix: sysctlbyname(f'vm.stats.vm.v_{suffix}', convert=to_int)
+        for suffix in suffixes
+    }
     memmax = stats['page_count']
     mem = (memmax - stats['free_count'] - stats['inactive_count'] - stats['cache_count'])
     free = int(100 * mem / memmax)
@@ -86,11 +139,13 @@ def memory():
 
 def cpu():
     global cpusage
-    items = [f'dev.cpu.{n}.temperature' for n in range(4)] + ['kern.cp_time']
-    lines = sp.check_output(['sysctl'] + items, encoding='ascii').splitlines()
-    temps = [float(ln.split()[1][:-1]) for ln in lines[:-1]]
+    temps = [
+        sysctlbyname(f'dev.cpu.{n}.temperature', convert=to_degC)
+        for n in range(4)
+    ]
     T = round(stat.mean(temps))
-    states = [int(j) for j in lines[-1].split()[1:]]
+    resbuf = sysctlbyname('kern.cp_time', buflen=40)
+    states = struct.unpack('5L', resbuf)
     # According to /usr/include/sys/resource.h, these are:
     # USER, NICE, SYS, INT, IDLE
     total = sum(states)
@@ -106,7 +161,7 @@ def date():
 
 def main():
     """
-    Entry point for i3-status.py.
+    Entry point for statusline-i3.py
     """
     while True:
         start = time.time()
@@ -114,7 +169,7 @@ def main():
         print(' | '.join(items))
         end = time.time()
         delta = end - start
-        # print(f"DEBUG: looptijd = {delta:.2f} s")
+        print(f"DEBUG: looptijd = {delta:.2f} s")
         if delta < 1:
             time.sleep(1 - delta)
 
@@ -123,6 +178,7 @@ def main():
 netdata = {'age0': (0, 0, 0), 'rl0': (0, 0, 0)}
 lastmail = {'count': -1, 'time': 0}
 cpusage = {'used': 0, 'total': 0}
+libc = ctypes.CDLL('libc.so.7')
 
 if __name__ == '__main__':
     main()
