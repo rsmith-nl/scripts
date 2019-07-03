c2399070a0772f62bc5661d32ea2a8ffc090e32f
Roland Smith <rsmith@xs4all.nl>
2019-07-03 22:16:09 +0200

    Remove most globals. Add comments.

--- statusline-i3.py
+++ statusline-i3.py
@@ -5,7 +5,7 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-06-30T22:23:11+0200
-# Last modified: 2019-07-03T00:13:37+0200
+# Last modified: 2019-07-03T22:14:40+0200
 """Generate status line for i3 on my machine."""
 
 import ctypes
@@ -16,7 +16,9 @@ import statistics as stat
 import struct
 import time
 
+# Global data
 __version__ = 1.999
+libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
 
 # Low level functions.
 
@@ -39,6 +41,9 @@ def sysctlbyname(name, buflen=4, convert=None):
         name (str): Name of the sysctl to query
         buflen (int): Length of the data buffer to use.
         convert: Optional function to convert the data.
+
+    Returns:
+        The requested binary data, converted if desired.
     """
     name_in = ctypes.c_char_p(bytes(name, encoding='ascii'))
     oldlen = ctypes.c_size_t(buflen)
@@ -60,6 +65,9 @@ def sysctl(name, buflen=4, convert=None):
         name: list or tuple of integers.
         buflen (int): Length of the data buffer to use.
         convert: Optional function to convert the data.
+
+    Returns:
+        The requested binary data, converted if desired.
     """
     cnt = len(name)
     mib = ctypes.c_int * cnt
@@ -80,55 +88,82 @@ def sysctl(name, buflen=4, convert=None):
 
 # High level functions
 
-def network():
-    global netdata
+def network(previous):
+    """
+    Report on bytes in/out for the network interfaces.
+
+    Arguments:
+        previous: A dict of {interface: (inbytes, outbytes, time)}.
+
+    Returns:
+        A new dict of {interface: (inbytes, outbytes, time)}, and a formatted
+        string to display.
+    """
     cnt = sysctlbyname('net.link.generic.system.ifcount', convert=to_int)
+    newdata = {}
     items = []
     for n in range(1, cnt):
         tm = time.time()
         data = sysctl([4, 18, 0, 2, n, 1], buflen=208)
         name = data[:16].strip(b'\x00').decode('ascii')
         ibytes = to_int(data[120:128])
-        obytes = to_int(data[128:138])
-        dt = tm - netdata[name][2]
-        d_in = int((ibytes - netdata[name][0])/dt)
-        d_out = int((obytes - netdata[name][1])/dt)
-        netdata[name] = (ibytes, obytes, tm)
-        items.append(f'{name}: {d_in}B/{d_out}B')
-    return ' '.join(items)
+        obytes = to_int(data[128:136])
+        # print("DEBUG: ibytes, obytes = ", ibytes, obytes)
+        if previous:
+            dt = tm - previous[name][2]
+            d_in = int((ibytes - previous[name][0])/dt)
+            d_out = int((obytes - previous[name][1])/dt)
+            items.append(f'{name}: {d_in}B/{d_out}B')
+        else:
+            items.append(f'{name}: 0B/0B')
+        newdata[name] = (ibytes, obytes, tm)
+    return newdata, '  '.join(items)
 
 
-def mail():
+def mail(previous):
+    """
+    Report unread mail.
+
+    Arguments:
+        previous: a 2-tuple (unread, time) from the previous call, or None
+
+    Returns: A new 2-tuple (unread, time) and a string to display.
+    """
     mboxname = '/home/rsmith/Mail/received'
-    global lastmail
     newtime = os.stat(mboxname).st_mtime
-    if newtime > lastmail['time']:
+    if previous is None or newtime > previous[1]:
         with open(mboxname) as mbox:
-            mm = mmap.mmap(mbox.fileno(), 0, prot=mmap.PROT_READ)
-            start, total = 0, 0
-            while True:
-                rv = mm.find(b'\n\nFrom ', start)
-                if rv == -1:
-                    break
-                else:
-                    total += 1
-                    start = rv+7
-            start, read = 0, 0
-            while True:
-                rv = mm.find(b'\nStatus: R', start)
-                if rv == -1:
-                    break
-                else:
-                    read += 1
-                    start = rv+10
-        mailcount = total - read
-        lastmail = {'count': mailcount, 'time': newtime}
+            with mmap.mmap(mbox.fileno(), 0, prot=mmap.PROT_READ) as mm:
+                start, total = 0, 1  # First mail is not found; it starts on first line...
+                while True:
+                    rv = mm.find(b'\n\nFrom ', start)
+                    if rv == -1:
+                        break
+                    else:
+                        total += 1
+                        start = rv+7
+                start, read = 0, 0
+                while True:
+                    rv = mm.find(b'\nStatus: R', start)
+                    if rv == -1:
+                        break
+                    else:
+                        read += 1
+                        start = rv+10
+        unread = total - read
+        newdata = (unread, newtime)
     else:
-        mailcount = lastmail['count']
-    return f'Mail: {mailcount}'
+        unread = previous[0]
+        newdata = previous
+    return newdata, f'Mail: {unread}'
 
 
 def memory():
+    """
+    Report on the RAM usage on FreeBSD.
+
+    Returns: a formatted string to display.
+    """
     suffixes = ('page_count', 'free_count', 'inactive_count', 'cache_count')
     stats = {
         suffix: sysctlbyname(f'vm.stats.vm.v_{suffix}', convert=to_int)
@@ -140,8 +175,16 @@ def memory():
     return f'RAM: {free}%'
 
 
-def cpu():
-    global cpusage
+def cpu(previous):
+    """
+    Report the CPU usage and temperature.
+
+    Argument:
+        previous: A 2-tuple (used, total) from the previous run.
+
+    Returns:
+        A 2-tuple (used, total) and a string to display.
+    """
     temps = [
         sysctlbyname(f'dev.cpu.{n}.temperature', convert=to_degC)
         for n in range(4)
@@ -153,12 +196,16 @@ def cpu():
     # USER, NICE, SYS, INT, IDLE
     total = sum(states)
     used = total - states[-1]
-    frac = int((used - cpusage['used']) / (total - cpusage['total']) * 100)
-    cpusage = {'used': used, 'total': total}
-    return f'CPU: {frac}%, {T}°C'
+    if previous:
+        prevused, prevtotal = previous
+        frac = int((used - prevused) / (total - prevtotal) * 100)
+    else:
+        frac = 0
+    return (used, total), f'CPU: {frac}%, {T}°C'
 
 
 def date():
+    """Return the date formatted for display."""
     return time.strftime('%a %Y-%m-%d %H:%M:%S')
 
 
@@ -166,22 +213,22 @@ def main():
     """
     Entry point for statusline-i3.py
     """
+    netdata, _ = network(None)
+    maildata, _ = mail(None)
+    cpusage, _ = cpu(None)
+    time.sleep(0.1)  # LEst we get divide by zero in cpu()
     while True:
         start = time.time()
-        items = [network(), mail(), memory(), cpu(), date()]
-        print(' | '.join(items))
+        netdata, netstr = network(netdata)
+        maildata, mailstr = mail(maildata)
+        cpusage, cpustr = cpu(cpusage)
+        print(' | '.join([netstr, mailstr, memory(), cpustr, date()]))
         end = time.time()
         delta = end - start
-        print(f"DEBUG: looptijd = {delta:.2f} s")
+        # print(f"DEBUG: cycle time = {delta:.3f} s")
         if delta < 1:
             time.sleep(1 - delta)
 
-
-# Global data
-netdata = {'age0': (0, 0, 0), 'rl0': (0, 0, 0)}
-lastmail = {'count': -1, 'time': 0}
-cpusage = {'used': 0, 'total': 0}
-libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
 
 if __name__ == '__main__':
     main()
