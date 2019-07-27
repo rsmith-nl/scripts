e2959e0e5c9b89a42cc4df451a75b5427edc85e3
Roland Smith <rsmith@xs4all.nl>
2019-07-27 14:49:26 +0200

    More error checking. Better messages.

--- checkfor.py
+++ checkfor.py
@@ -4,10 +4,10 @@
 # Copyright © 2012-2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2012-12-13T01:39:03+01:00
-# Last modified: 2018-07-07T13:06:25+0200
+# Last modified: 2019-07-27T14:47:51+0200
 
 import logging
-import subprocess
+import subprocess as sp
 import sys
 
 
@@ -26,11 +26,17 @@ def checkfor(args, rv=0):
         if ' ' in args:
             raise ValueError('no spaces in single command allowed')
         args = [args]
+    else:
+        if not isinstance(args, (list, tuple)):
+            raise ValueError('args should be a list or tuple')
+        if not all(isinstance(x, str) for x in args):
+            raise ValueError('args should be a list or tuple of strings')
     try:
-        rc = subprocess.call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        if rc != rv:
-            raise OSError
-        logging.info(f'found required program "{args[0]}"')
-    except OSError as oops:
+        cp = sp.run(args, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    except FileNotFoundError as oops:
         logging.error(f'required program "{args[0]}" not found: {oops.strerror}.')
         sys.exit(1)
+    if cp.returncode != rv:
+        logging.error(f'returncode {cp.returncode} should be {rv}')
+        sys.exit(1)
+    logging.info(f'found required program "{args[0]}"')
