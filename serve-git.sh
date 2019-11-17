423d1e7a23c1810c48d28c34a886eddc71804ba3
Roland Smith <rsmith@xs4all.nl>
2019-11-17 14:52:52 +0100

    Explicitly specifiy listen hostnames.

--- serve-git.sh
+++ serve-git.sh
@@ -4,10 +4,10 @@
 # Starts the git daemon for all git repositories under the current working
 # directory.
 #
-# Copyright © 2015-2016 R.F. Smith <rsmith@xs4all.nl>.
+# Copyright © 2015,2016,2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2016-03-19T10:35:02+0100
-# Last modified: 2018-04-16T22:33:31+0200
+# Last modified: 2019-11-17T14:48:55+0100
 
 # Check for special programs that are used in this script.
 PROGS="git"
@@ -21,9 +21,10 @@ done
 
 set -eu
 WD=$(pwd)
+H=$(hostname)
 GITDIRS=$(find $WD -type d -name '.git'|sort)
 echo "Starting git-daemon for the following directories:"
 echo $GITDIRS|fmt
 echo "Press CTRL-C to quit."
-git daemon --reuseaddr --verbose --export-all --base-path=/home/rsmith $GITDIRS
+git daemon --reuseaddr --verbose --export-all --listen=${H} --base-path=/home/rsmith $GITDIRS
 
