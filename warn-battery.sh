d938b553ead8177db444c1404ebfa9d893f4c5cb
Roland Smith <rsmith@xs4all.nl>
2020-07-12 14:19:22 +0200

    Discard mixer output.

--- warn-battery.sh
+++ warn-battery.sh
@@ -7,7 +7,7 @@
 #
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # Created: 2020-07-12T13:53:59+0200
-# Last modified: 2020-07-12T13:58:33+0200
+# Last modified: 2020-07-12T14:19:02+0200
 
 
 # Get the battery percentage
@@ -16,8 +16,8 @@ BAT=`sysctl -n hw.acpi.battery.life`
 VOL=`mixer vol | sed -e 's/^.*://'`
 if [ $BAT -le 10 ]; then
     # Set sound level and play sound
-    mixer vol 50
+    mixer vol 50 >/dev/null
     out123 --wave-freq 400 --wave-sweep 800 --sweep-count 5 --wave-pat square
     # Restore sound volume.
-    mixer vol $VOL
+    mixer vol $VOL >/dev/null
 fi
