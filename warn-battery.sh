5fea26e812a6ce8db87cb27024e258b11eb2540e
Roland Smith <rsmith@xs4all.nl>
2020-07-12 14:56:43 +0200

    Only warn if battery is discharging.

--- warn-battery.sh
+++ warn-battery.sh
@@ -7,16 +7,18 @@
 #
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # Created: 2020-07-12T13:53:59+0200
-# Last modified: 2020-07-12T14:32:12+0200
+# Last modified: 2020-07-12T14:56:15+0200
 
+# Locations of binaries.
 MIXER=/usr/sbin/mixer
 OUT=/usr/local/bin/out123
 
-# Get the battery percentage
+# Get the battery percentage and state
 BAT=`sysctl -n hw.acpi.battery.life`
+STATE=`sysctl -n hw.acpi.battery.state`
 # Get current sound volume
 VOL=`mixer vol | sed -e 's/^.*://'`
-if [ $BAT -le 10 ]; then
+if [ $STATE -eq 1 -a $BAT -le 10 ]; then
     # Set sound level and play sound
     $MIXER vol 50 >/dev/null
     $OUT --wave-freq 400 --wave-sweep 800 --sweep-count 5 --wave-pat square
