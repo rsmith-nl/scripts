12f16a8a7f926d6ff70a0e91c4f9741a2aaebb50
Roland Smith <rsmith@xs4all.nl>
2020-07-12 13:59:37 +0200

    Script to warn when battery is low.

---
+++ warn-battery.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# file: warn-battery.sh
+# Script to sound a warning when the battery becomes low. To be run from cron.
+# Requires the audio/mpg123 package.
+#
+# vim:fileencoding=utf-8:fdm=marker:ft=sh
+#
+# Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
+# Created: 2020-07-12T13:53:59+0200
+# Last modified: 2020-07-12T13:58:33+0200
+
+
+# Get the battery percentage
+BAT=`sysctl -n hw.acpi.battery.life`
+# Get current sound volume
+VOL=`mixer vol | sed -e 's/^.*://'`
+if [ $BAT -le 10 ]; then
+    # Set sound level and play sound
+    mixer vol 50
+    out123 --wave-freq 400 --wave-sweep 800 --sweep-count 5 --wave-pat square
+    # Restore sound volume.
+    mixer vol $VOL
+fi
