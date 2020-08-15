5cfb602acba5a10ccd6d2d4b6193bd611502fae0
Roland Smith <rsmith@xs4all.nl>
2020-08-15 14:21:59 +0200

    Update excluded files.

--- find-modified.sh
+++ find-modified.sh
@@ -5,7 +5,7 @@
 # Copyright © 2015-2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2015-05-08T22:12:45+02:00
-# Last modified: 2018-04-16T21:57:25+0200
+# Last modified: 2020-08-15T14:20:52+0200
 
 if [ -z $1 ]; then
    echo "Error: you must give a number of days as the argument."
@@ -15,7 +15,7 @@ if [ -z $1 ]; then
    exit 0
 fi
 
-EXCLUDED="/\..*|WWW/output|tmp"
+EXCLUDED="/\..*|tmp"
 
 cd $HOME
 find . -mtime -$1 -type f |grep -Ev $EXCLUDED|cut -c 3-
