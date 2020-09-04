9d67d70009cead36101d6ea2a92ac3e65c1a8ada
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-09-04 17:12:30 +0900

    replaced if-fi syntaxes with case-esac and ||
    TBH what sed has -I option...?

--- fixbb.sh
+++ fixbb.sh
@@ -8,28 +8,28 @@
 # Last modified: 2018-04-16T21:58:58+0200
 
 # Check for arguments
-if [ $# -eq 0 ]; then
+case $# in 0)
     echo "Usage: $(basename $0) filename"
     exit 1
-fi
+esac
 
-if [ ! -f "$1" ]; then
-    echo "File "$1" doesn't exist!"
+[ ! -f "$1" ] || {
+    echo "$File ${1} doesn't exist!"
     exit 0
-fi
+}
 
 # Check for special programs that are used in this script.
 P=gs
 which $P >/dev/null 2>&1
-if [ $? -ne 0 ]; then
+case $# in [!0]*) 
     echo "$(basename $0): The program \"$P\" cannot be found."
     exit 1
-fi
+esac
 
-if ! grep '%%Pages\?:.*1' >/dev/null $1; then
+grep '%%Pages\?:.*1' >/dev/null $1 || {
     echo "Sorry, this script only works for single-page documents!"
     exit 0
-fi
+}
 
 # Determine the new BoundingBox.
 # This only works properly for single-page documents!
