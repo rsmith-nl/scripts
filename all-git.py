6a4caa9d7839a096ca115b5b55e1a56326d2c82b
Roland Smith <rsmith@xs4all.nl>
2021-01-09 14:18:03 +0100

    Sorted all-git output.

--- all-git.py
+++ all-git.py
@@ -5,20 +5,28 @@
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2020-12-13T22:19:38+0100
-# Last modified: 2020-12-13T22:46:40+0100
+# Last modified: 2021-01-09T14:16:44+0100
 """Report on all subdirectories that are managed by git."""
 
 import os
 import subprocess as sp
+import time
 
+results = []
 for root, dirs, files in os.walk("."):
     if ".git" in dirs:
         rp = os.getcwd()
         os.chdir(root)
         data = sp.run(
-            ["git", "-P", "log", "-n", "1", "--pretty=%H %aI"],
+            ["git", "-P", "log", "-n", "1", "--pretty=%H %at"],
             stdout=sp.PIPE,
             text=True
         ).stdout[:-1]
+        commit_id, seconds = data.split()
+        seconds = int(seconds)
+        results.append((commit_id, seconds, root[2:]))
         os.chdir(rp)
-        print(data, root[2:])
+# Move the most recently changed to the end of the list.
+results.sort(key=lambda x: x[1])
+for commit_id, seconds, path in results:
+    print(commit_id, time.strftime("%Y-%m-%dT%H:%M%:%S%z", time.localtime(seconds)), path)
