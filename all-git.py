704475db2e9fb91fe56fb3a51e9c8d0d8cde5e5a
Roland Smith <rsmith@xs4all.nl>
2021-02-14 21:26:06 +0100

    Catch repo's without commits.

--- all-git.py
+++ all-git.py
@@ -5,10 +5,11 @@
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2020-12-13T22:19:38+0100
-# Last modified: 2021-01-09T14:16:44+0100
+# Last modified: 2021-02-14T21:24:18+0100
 """Report on all subdirectories that are managed by git."""
 
 import os
+import sys
 import subprocess as sp
 import time
 
@@ -20,11 +21,15 @@ for root, dirs, files in os.walk("."):
         data = sp.run(
             ["git", "-P", "log", "-n", "1", "--pretty=%H %at"],
             stdout=sp.PIPE,
+            stderr=sp.DEVNULL,
             text=True
         ).stdout[:-1]
-        commit_id, seconds = data.split()
-        seconds = int(seconds)
-        results.append((commit_id, seconds, root[2:]))
+        try:
+            commit_id, seconds = data.split()
+            seconds = int(seconds)
+            results.append((commit_id, seconds, root[2:]))
+        except ValueError:
+            print(f"ERROR in ‘{root}’, skipping.", file=sys.stderr)
         os.chdir(rp)
 # Move the most recently changed to the end of the list.
 results.sort(key=lambda x: x[1])
