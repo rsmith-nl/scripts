e37fbdbdb673b802aa6f71d50840e5fc4219762f
Roland Smith <rsmith@xs4all.nl>
2021-11-03 02:00:56 +0100

    Reformat tcl commands for DRAWEXE.

--- stepviewer.sh
+++ stepviewer.sh
@@ -6,12 +6,15 @@
 # Copyright © 2021 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2021-10-31T02:35:43+0200
-# Last modified: 2021-11-02T14:34:33+0100
+# Last modified: 2021-11-03T01:55:42+0100
 
 export CASROOT="/usr/local/OpenCAS"
-export CSF_OCCTResourcePath="/usr/local/OpenCAS/resources"
+export CSF_OCCTResourcePath="${CASROOT}/resources"
 export DRAWHOME="${CSF_OCCTResourcePath}/DrawResources"
 export CSF_DrawPluginDefaults="${CSF_OCCTResourcePath}/DrawResources"
 export DRAWDEFAULT="${CSF_OCCTResourcePath}/DrawResources/DrawDefault"
 
-DRAWEXE -i -c "pload ALL;vinit;stepread \"${1}\" a *;vsetdispmode 1;vdisplay a_1;vsetmaterial aluminium;vaxo;vfit"
+CMDS="pload ALL;vinit;stepread \"${1}\" a *;"
+CMDS="${CMDS}vsetdispmode 1;vdisplay a_1;"
+CMDS="${CMDS}vsetmaterial aluminium;vaxo;vfit"
+DRAWEXE -i -c ${CMDS}
