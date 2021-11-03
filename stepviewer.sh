4e7343ea2b60b0aa25a3dc56801427d72b7845b8
Roland Smith <rsmith@xs4all.nl>
2021-11-03 03:58:46 +0100

    Select the item in the step viewer.

--- stepviewer.sh
+++ stepviewer.sh
@@ -6,7 +6,7 @@
 # Copyright © 2021 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2021-10-31T02:35:43+0200
-# Last modified: 2021-11-03T01:55:42+0100
+# Last modified: 2021-11-03T03:16:48+0100
 
 export CASROOT="/usr/local/OpenCAS"
 export CSF_OCCTResourcePath="${CASROOT}/resources"
@@ -15,6 +15,6 @@ export CSF_DrawPluginDefaults="${CSF_OCCTResourcePath}/DrawResources"
 export DRAWDEFAULT="${CSF_OCCTResourcePath}/DrawResources/DrawDefault"
 
 CMDS="pload ALL;vinit;stepread \"${1}\" a *;"
-CMDS="${CMDS}vsetdispmode 1;vdisplay a_1;"
+CMDS="${CMDS}vsetdispmode 1;vdisplay a_1;vchangeselected a_1;"
 CMDS="${CMDS}vsetmaterial aluminium;vaxo;vfit"
 DRAWEXE -i -c ${CMDS}
