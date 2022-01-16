b1840a4dcb2429e05962aca2d362d1fdc6695a5c
Roland Smith <rsmith@xs4all.nl>
2022-01-16 14:37:09 +0100

    Calculate password length based on entropy.

--- genpw.py
+++ genpw.py
@@ -5,11 +5,11 @@
 # Copyright © 2013-2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2013-12-11T23:33:07+01:00
-# Last modified: 2021-05-22T18:19:09+0200
+# Last modified: 2022-01-16T14:36:27+0100
 """
 Generate random passwords.
 
-The passwords are random data from ``os.urandom`` which is encoded in a chosen
+The passwords are random data from ``secrets.token_bytes`` which is encoded in a chosen
 format.
 """
 
@@ -17,63 +17,18 @@ import argparse
 import base64
 import secrets
 import sys
+import math
+import logging
 
-__version__ = "2021.05.22"
-
-
-def main():
-    """Entry point for genpw."""
-    args = setup()
-    encoder_choice = {
-        "base64": base64.b64encode,
-        "base64-urlsafe": base64.urlsafe_b64encode,
-        "ascii85": base64.a85encode,
-        "base85": base64.b85encode,
-    }
-    encoder = encoder_choice[args.encoding]
-    for _ in range(args.repeat):
-        pw = genpw(args.length, encoder)
-        if args.groupby > 0:
-            n = args.groupby
-            count = len(pw) // n
-            pw = " ".join([pw[k : k + n] for k in range(0, n * count, n)])
-        print(pw)
+__version__ = "2022.01.16"
 
 
-def setup():
-    """Process command-line arguments."""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "-l",
-        "--length",
-        default=64,
-        type=int,
-        help="# of random characters for password (default 64)",
-    )
-    parser.add_argument(
-        "-e",
-        "--encoding",
-        choices=["base64", "base64-urlsafe", "ascii85", "base85"],
-        default="base64-urlsafe",
-        help="encoding for password (default base64-urlsafe)",
-    )
-    parser.add_argument(
-        "-r",
-        "--repeat",
-        default=1,
-        type=int,
-        help="number of passwords to generate (default: 1)",
-    )
-    parser.add_argument(
-        "-g",
-        "--groupby",
-        default=0,
-        metavar="N",
-        type=int,
-        help="group by N characters (default: 0; no grouping)",
-    )
-    parser.add_argument("-v", "--version", action="version", version=__version__)
-    return parser.parse_args(sys.argv[1:])
+def shannon(n):
+    """
+    Calculate the Shannon entropy of an alphabet of size n, where every
+    character has the same chance of being selected.
+    """
+    return -(1/n)*math.log(1/n, 2)*n
 
 
 def genpw(length, encoder):
@@ -91,5 +46,70 @@ def genpw(length, encoder):
     return encoder(d).decode()[:length]
 
 
-if __name__ == "__main__":
-    main()
+parser = argparse.ArgumentParser(description=__doc__)
+edef = 70
+parser.add_argument(
+    "--log",
+    default="warning",
+    choices=["debug", "info", "warning", "error"],
+    help="logging level (default: warning)",
+)
+# The idea of using entropy is based on
+# https://pthree.org/2017/09/04/a-practical-and-secure-password-and-passphrase-generator/
+parser.add_argument(
+    "-e",
+    "--entropy",
+    default=edef,
+    type=int,
+    help=f"bits of entropy of password (default {edef})",
+)
+cdef = "base85"
+parser.add_argument(
+    "-c",
+    "--encoding",
+    choices=["base64", "base64-urlsafe", "ascii85", "base85"],
+    default=cdef,
+    help=f"encoding for password (default {cdef})",
+)
+rdef = 1
+parser.add_argument(
+    "-r",
+    "--repeat",
+    default=rdef,
+    type=int,
+    help=f"number of passwords to generate (default: {rdef})",
+)
+gdef = 0
+parser.add_argument(
+    "-g",
+    "--groupby",
+    default=gdef,
+    metavar="N",
+    type=int,
+    help=f"group by N characters (default: {gdef}, 0 = no grouping)",
+)
+parser.add_argument("-v", "--version", action="version", version=__version__)
+args = parser.parse_args(sys.argv[1:])
+logging.basicConfig(
+    level=getattr(logging, args.log.upper(), None), format="%(levelname)s: %(message)s"
+)
+
+encoder_choice = {
+    "base64": (base64.b64encode, 64),
+    "base64-urlsafe": (base64.urlsafe_b64encode, 64),
+    "base85": (base64.b85encode, 85),
+}
+encoder, alphabetsize = encoder_choice[args.encoding]
+se = shannon(alphabetsize)
+logging.info(f"{args.encoding} encoder is used, {se:.3f} bits/character entropy")
+length = math.ceil(args.entropy/se)
+logging.info(f"{length} characters required for ≥{args.entropy} bits of entropy")
+if args.groupby:
+    logging.info(f"grouping by {args.groupby} characters")
+for _ in range(args.repeat):
+    pw = genpw(length, encoder)
+    if args.groupby > 0:
+        n = args.groupby
+        count = math.ceil(len(pw) / n)
+        pw = " ".join([pw[k : k + n] for k in range(0, n * count, n)])
+    print(pw)
