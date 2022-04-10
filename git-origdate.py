61c3713b96933a331187aa5222217e7c0cea4f86
Roland Smith <rsmith@xs4all.nl>
2022-04-10 13:55:01 +0200

    Implement argument parsing for git-origdate.

--- git-origdate.py
+++ git-origdate.py
@@ -5,23 +5,23 @@
 # Copyright © 2015-2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2015-01-03T16:31:09+01:00
-# Last modified: 2020-12-16T00:25:34+0100
-"""Report when arguments were checked into git."""
+# Last modified: 2022-04-10T12:56:09+0200
+"""Report when files named on the command line were checked into git."""
 
-import os.path
+import argparse
+import logging
+import os
 import subprocess as sp
 import sys
 
-__version__ = "2020.12.16"
+__version__ = "2022.04.10"
 
-if len(sys.argv) == 1:
-    binary = os.path.basename(sys.argv[0])
-    print(f"{binary} ver. {__version__}", file=sys.stderr)
-    print(f"Usage: {binary} [file ...]", file=sys.stderr)
-    sys.exit(0)
 
-try:
-    for fn in sys.argv[1:]:
+def main():
+    args = setup()
+    origdir = os.getcwd()
+    for path in args.files:
+        dirname, filename = os.path.split(path)
         args = [
             "git",
             "--no-pager",
@@ -29,15 +29,59 @@ try:
             "--diff-filter=A",
             "--format=%ai",
             "--",
-            fn,
+            filename,
         ]
-        cp = sp.run(args, stdout=sp.PIPE, stderr=sp.DEVNULL, text=True, check=True)
+        if dirname:
+            logging.info(f"changing to directory {dirname}")
+            os.chdir(dirname)
+        cp = sp.run(args, stdout=sp.PIPE, stderr=sp.DEVNULL, text=True)
+        logging.debug(f"file {path}, returncode = {cp.returncode}")
+        os.chdir(origdir)
+        if cp.returncode == 128:
+            logging.warning(f"“{path}” not in a git repository; skipping")
+            continue
+        elif cp.returncode != 0:
+            logging.warning(f"git returned {cp.returncode}; skipping “{path}”")
+            continue
+        dates = cp.stdout.strip().splitlines()
+        if len(dates) > 1:
+            logging.debug(f"multiple dates returned for {path}:")
+            for d in dates:
+                logging.debug(f"{d}")
         # Sometimes this git command will return *multiple dates*!
         # In that case, select the oldest.
-        date = cp.stdout.strip().splitlines()[-1]
-        print(f'"{fn}": {date}')
-except sp.CalledProcessError as e:
-    if e.returncode == 128:
-        print("Not a git repository! Exiting.")
-    else:
-        print(f"git error: '{e.strerror}'. Exiting.")
+        print(f'{path}: {dates[-1]}')
+
+
+def setup():
+    """Parse command-line arguments. Check for required programs."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--log",
+        default="info",
+        choices=["debug", "info", "warning", "error"],
+        help="logging level (defaults to 'info')",
+    )
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument(
+        "files", nargs="*", help="one or more files to process"
+    )
+    args = parser.parse_args(sys.argv[1:])
+    logging.basicConfig(
+        level=getattr(logging, args.log.upper(), None),
+        format="%(levelname)s: %(message)s",
+    )
+    logging.debug(f"Command line arguments = {sys.argv}")
+    logging.debug(f"Parsed arguments = {args}")
+    # Check for required programs.
+    try:
+        sp.run(["git"], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+        logging.debug("found “git”")
+    except FileNotFoundError:
+        logging.error("the program “git” cannot be found")
+        sys.exit(1)
+    return args
+
+
+if __name__ == "__main__":
+    main()
