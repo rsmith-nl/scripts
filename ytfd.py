46102ab053b89f123486e82d59824f8920665733
Roland Smith <rsmith@xs4all.nl>
2022-07-17 12:07:19 +0200

    Add downloader and settings.

--- ytfd.py
+++ ytfd.py
@@ -5,13 +5,8 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-07-28T13:42:58+0200
-# Last modified: 2021-08-11T00:07:03+0200
-"""Get the latest video's from your favorite youtube channels.
-
-    This script now generates commands for the mpv viewer that force it
-    to use youtube-dl.  This was done so I could limit the quality of the
-    downloaded clips to my screen resolution.
-"""
+# Last modified: 2022-07-17T12:06:28+0200
+"""View or download the latest video's from your favorite youtube channels."""
 
 import datetime
 import html
@@ -29,6 +24,9 @@ now = datetime.datetime.now(tz=datetime.timezone.utc)
 # {
 #     "limit": 7,
 #     "viewer": "mpv",
+#     "viewer-args": "",
+#     "downloader": "yt-dlp",
+#     "downloader-args": "-S res:720 --",
 #     "channels": {
 #         "first channel name": "UC7QoixBiFO2zstyEXkbVpVg",
 #         "second channel name": "UCvY8pgX_3ksKvZBNFHVoUAQ",
@@ -36,21 +34,38 @@ now = datetime.datetime.now(tz=datetime.timezone.utc)
 #     }
 # }
 # The “limit” is optional. If not present, it will be set to 7 days.
-# The "viewer" is also optional. If not present, it will be set to 'mpv'.
+# "viewer", “viewer-args”, “downloader” and “downloader-args” are also optional.
+# If not present, it will be set with the default values.
 # Note that the channelId's above are not real, they're randomly generated!
 # You can find the “channelId” in the source of the channel's homepage.
 rcpath = os.environ["HOME"] + os.sep + ".ytfdrc"
+
+
+def _check(settings, key, tp, default):
+    """
+    Verify that a setting exists and is the right type and return it.
+    If not, return the default value.
+    """
+    if key not in settings:
+        return default
+    if not isinstance(settings[key], tp):
+        return default
+    return settings[key]
+
+
 with open(rcpath) as rc:
     settings = json.load(rc)
+    # Unpack data for use. Other keys are ignored.
     channels = settings["channels"]
-    if "limit" in settings:
-        limit = settings["limit"]
-    else:
-        limit = 7
-    if "viewer" in settings:
-        viewer = settings["viewer"]
-    else:
-        viewer = "mpv"
+    limit = _check(settings, "limit", int, 7)
+    viewer = _check(settings, "viewer", str, "mpv")
+    viewer_args = _check(settings, "viewer-args", str, "")
+    if viewer_args:
+        viewer = " ".join([viewer, viewer_args])
+    downloader = _check(settings, "downloader", str, "yt-dlp")
+    downloader_args = _check(settings, "downloader-args", str, "-S res:720 --")
+    if downloader_args:
+        downloader = " ".join([downloader, downloader_args])
 
 
 # Flush after every line.
@@ -75,7 +90,7 @@ for channel_title, channel_id in channels.items():
         for pt in re.findall("<published>(.*)</published>", text, re.IGNORECASE)
     ]
     items = tuple(
-        (html.unescape(title), link, date)
+        (html.unescape(title), link.rsplit("=")[1], date)
         for title, link, date in zip(titles, links, published)
         if "watch" in link and (now - date).days < limit
     )
@@ -86,5 +101,6 @@ for channel_title, channel_id in channels.items():
     for title, link, date in items:
         print(f"∙ title: “{title}”")
         print(f"   date: {date}")
-        print(f"   view: {viewer} '{link}' &")
+        print(f"   view: {viewer} 'https://www.youtube.com/watch?v={link}' &")
+        print(f"   download: {downloader} {link}")
     print()
