c3d4c57e3013f05cdd3ec3851435df4d9c939cb2
Roland Smith <rsmith@xs4all.nl>
2025-08-04 09:48:27 +0200

    Fix warn-battery.

--- warn-battery.sh
+++ warn-battery.sh
@@ -8,7 +8,7 @@
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2020-07-12T13:53:59+0200
-# Last modified: 2021-02-28T20:48:30+0100
+# Last modified: 2025-08-04T09:47:53+0200
 
 set -e
 
@@ -20,11 +20,11 @@ OUT=/usr/local/bin/out123
 BAT=`sysctl -n hw.acpi.battery.life`
 STATE=`sysctl -n hw.acpi.battery.state`
 # Get current sound volume
-VOL=`mixer vol | sed -e 's/^.*://'`
+VOL=`mixer vol.volume | sed 's/^.*://'`
 if [ $STATE -eq 1 -a $BAT -le 10 ]; then
     # Set sound level and play sound
-    $MIXER vol 50 >/dev/null
+    $MIXER vol.volume=0.50 >/dev/null
     $OUT --wave-freq 400 --wave-sweep 800 --sweep-count 5 --wave-pat square
     # Restore sound volume.
-    $MIXER vol $VOL >/dev/null
+    $MIXER vol.volume=$VOL >/dev/null
 fi
